fd9ed446a51f5f73a747f572f974cfdf2dcd08f7
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-10-22 08:28:38 -0700

    Gulp task changes. Rebuilt React 'pages' for 0.13.2 -> 0.14.0 upgrade

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var browserify = require('browserify');
+var streamify = require('gulp-streamify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var minify = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util')
+var argv = require('yargs').argv;
+var gzip = require('gulp-gzip');
+var gulp = require('gulp');
+
+/*
+	css
+	- concats css files
+	- minifies / gzip
+*/
+gulp.task('css', function() {
+	var sources = [
+		'./css/grid.css', './css/main.css', './css/dashboard.css', './css/step-form.css'
+	];
+	
+	return gulp.src(sources)
+		.pipe(concat('style.css'))
+		.pipe(minify())
+		//.pipe(gzip())
+		.pipe(gulp.dest('./public/css'));
+});
+
+/*
+	react
+	- bundles React componenents
+	- converts JSX -> pure React
+	- minifies / gzip
+	@params
+	- file // React "page" component name, no file extension
+*/
+gulp.task('react', function() {
+	// Add JSX transformer to Browserify
+	var b = browserify();
+	b.transform(reactify);
+	b.add('./components/' + argv.file + '.jsx');
+	
+	// Bundle React components and minify JS
+	return b.bundle()
+		.pipe(source(argv.file + '.js'))
+		.pipe(streamify(uglify({
+			mangle: false,
+			compress: {
+				unused: false
+			}
+		}).on('error', gutil.log)))
+		//.pipe(gzip({append: false}))
+		.pipe(gulp.dest('./public/js/react/'));
+});
