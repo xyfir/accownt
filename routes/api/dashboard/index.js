1c0195bdce8b6ccaaabb9a01c2a6862d048e69bd
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-12-14 11:59:01 -0800

    Made api/dashboard/ads routes available.

--- routes/api/dashboard/index.js
+++ routes/api/dashboard/index.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
 
-//router.use('/ads', require('./ads'));
+router.use('/ads', require('./ads'));
 router.use('/account', require('./account'));
 router.use('/profiles', require('./profiles'));
 router.use('/security', require('./security'));
