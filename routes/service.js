855ee4f514ced207fac337fb8ec6d2b52f45a233
theycallmexshadow <theycallmexshadow@bitbucket.org>
2015-12-18 15:21:27 -0800

    Route modification.

--- routes/service.js
+++ routes/service.js
@@ -2,6 +2,6 @@ var router = require('express').Router();
 var service = require('../controllers/service');
 
 // accounts.xyfir.com/service/dashboard
-router.get('/dashboard', service.view);
+router.get('/dashboard/*', service.view);
 
 module.exports = router;
