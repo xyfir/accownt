51af29bc23e3ffadae1fa77ddb39970705bc7cd6
Mr. Xyfir <mr@xyfir.com>
2016-06-08 09:45:41 -0700

    Add request module

---
+++ client/lib/request.js
@@ -0,0 +1,38 @@
+export default function (options) {
+    let request = new XMLHttpRequest();
+    
+    // Set method and URL
+    request.open(options.method || "GET", options.url, true);
+    
+    // Handle response
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            switch (options.dataType) {
+                case "text":
+                    options.success(this.responseText, this.status); break;
+                default:
+                    options.success(JSON.parse(this.responseText), this.status);
+            }
+        }
+        else if (options.error) {
+            options.error(this.responseText, this.status);
+        }
+    };
+    
+    // Send request + data
+    if (options.method == undefined || options.method == "GET") {
+        request.send();
+    }
+    else {
+        let query = "";
+        
+        for (let key in options.data) {
+            query += encodeURIComponent(key) + "=" + encodeURIComponent(options.data[key]) + "&";
+        }
+        
+        query = query.substr(0, query.length - 1);
+        
+        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        request.send(query);
+    }
+};
