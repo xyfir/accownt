2550dd829894bd9abc9a2e12af924a7b68352141
Mr. Xyfir <mr@xyfir.com>
2016-06-08 11:39:14 -0700

    Split ads.js into info/update

---
+++ controllers/dashboard/ads/info.js
@@ -0,0 +1,33 @@
+const request = require("request");
+const config = require("../../../config");
+const db = require("../../../lib/db");
+	
+/*
+    GET api/dashboard/ads
+    RETURN
+        {info: string/json-string}
+*/
+module.exports = function(req, res) {
+    
+    db(cn => {
+        cn.query("SELECT xad_id FROM users WHERE id = ?", [req.session.uid], (err, rows) => {
+            cn.release();
+            
+            if (err || !rows.length) {
+                res.json({info: ""});
+                return;
+            }
+            
+            request({
+                url: config.xads + "api/xad-id/" + req.session.uid + "/" + rows[0].xad_id
+                    + "?secret=" + config.keys.xadid,
+            }, (err, response, body) => {
+                if (err)
+                    res.json({info: ""});
+                else
+                    res.json({info: JSON.parse(body).info});
+            });
+        });
+    });
+
+}
