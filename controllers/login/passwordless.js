0863b38edfb48669c0fdced4a0ceddfd24599c33
Mr. Xyfir <mr@xyfir.com>
2016-06-09 10:23:35 -0700

    Add controller for GET api/login/passwordless/:uid/:auth

---
+++ controllers/login/passwordless.js
@@ -0,0 +1,21 @@
+const validate = require("../../lib/auth/validateToken");
+const db = require("../../lib/db");
+
+/*
+    GET api/login/passwordless/:uid/:auth
+    DESCRIPTION
+        Attempts to login user with :uid/:auth
+*/
+module.exports = function(req, res) {
+
+    validate([req.params.uid], req.params.auth, isValid => {
+        if (isValid) {
+            req.session.uid = req.params.uid;
+            res.redirect(req.session.redirect ? req.session.redirect : "/app/#/dashboard");
+        }
+        else {
+            res.redirect("/app/#/login");
+        }
+    });
+
+}
