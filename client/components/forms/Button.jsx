f01fba5ad78e3c2d189ac4be4742cbfc9c2e97a2
Mr. Xyfir <mr@xyfir.com>
2016-06-09 18:12:25 -0700

    Update code to ES6

--- client/components/forms/Button.jsx
+++ client/components/forms/Button.jsx
@@ -1,13 +1,19 @@
-module.exports = React.createClass({
-	getDefaultProps: function() {
-		return {type: "primary"};
-	},
+import React from "react";
 
-	render: function() { 
+export default class Button extends React.Component {
+	
+	constructor(props) {
+		super(props);
+
+		this.state = { type: "primary" };
+	}
+
+	render() { 
 		return (
 			<button className={"btn-" + this.props.type} onClick={this.props.onClick}>
 				{this.props.children}
 			</button>
 		);
 	}
-});
+
+}
--- client/components/misc/Alert.jsx
+++ client/components/misc/Alert.jsx
@@ -1,14 +1,14 @@
-module.exports = React.createClass({
-	getDefaultProps: function() {
-		return {type: "info"};
-	},
+import React from "react";
+
+export default class Alert extends React.Component {
 	
-	render: function() {
+	render() {
 		return (
-			<div className={"alert alert-" + this.props.type}>
+			<div className={"alert alert-" + (this.props.type || "info")}>
 				<h3>{this.props.title}</h3>
 				<p>{this.props.children}</p>
 			</div>
 		);
 	}
-});
+
+}
