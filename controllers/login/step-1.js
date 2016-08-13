fd36e5be2f23322c0933eb7c072bc1a8c1e3da49
MrXyfir <mr@xyfir.com>
2016-08-13 08:01:13 -0700

    Limit login attempts for incorrect passwords

--- controllers/login/step-1.js
+++ controllers/login/step-1.js
@@ -9,7 +9,7 @@ const db = require("../../lib/db");
         {
             error: bool, message?: string, loggedIn?: bool,
             redirect?: string, uid?: number, auth?: string,
-            security?: { ** }
+            security?: { ** }, loginAttempts?: number
         }
 */
 module.exports = function(req, res) {
@@ -21,38 +21,66 @@ module.exports = function(req, res) {
     }
     
     db(cn => {
-        cn.query("SELECT id, password, verified FROM users WHERE email = ?", [req.body.email], (err, rows) => {
+        let sql = `
+            SELECT
+                id, email, password, verified, login_attempts,
+                DATE_ADD(
+                    last_login_attempt, INTERVAL 15 MINUTE
+                ) < NOW() as reset_attempts
+            FROM users WHERE email = ?
+        `; 
+        cn.query(sql, [req.body.email], (err, rows) => {
             
             // Check if user exists
             if (rows.length == 0) {
-                res.json({error: true, message: "Could not find a user with that email / password"});
-                return;
+                res.json({
+                    error: true,
+                    message: "Could not find a user with that email / password"
+                }); return;
             }
             
             // Verify password
             bcrypt.compare(req.body.password, rows[0].password, (err, match) => {
+
+                console.log("verify pass", rows[0]);
                 
                 // Password is incorrect
-                if (!match) {
-                    res.json({error: true, message: "Could not find a user with that email / password"});
-                    return;
+                // or login was correct but user hit login attempts limit
+                if (!match || (rows[0].login_attempts >= 5 && !rows[0].reset_attempts)) {
+                    // Increment login_attempts, update last_login_attempt
+                    sql = `
+                        UPDATE users SET
+                            login_attempts = login_attempts + 1,
+                            last_login_attempt = NOW()
+                        WHERE id = ?
+                    `;
+                    cn.query(sql, [rows[0].id], (e, r) => 0);
+
+                    res.json({
+                        error: true,
+                        message: "Could not find a user with that email / password",
+                        loginAttempts: rows[0].login_attempts + 1
+                    }); return;
                 }
+
+                // Clear login attempts
+                sql = "UPDATE users SET login_attempts = 0 WHERE id = ?";
+                cn.query(sql, [rows[0].id], (e, r) => 0);
                 
                 const uid = rows[0].id;
                 
-                // Check if account"s email is verified
+                // Check if account's email is verified
                 if (rows[0].verified == 0) {
                     require("../../lib/email/sendVerification")(uid, req.body.email);
                     res.json({
                         error: true,
                         message: "You cannot login until you've verified your email. A new verification link has been sent to your email."
-                    });
-                    return;
+                    }); return;
                 }
                 
                 // Check for extra security required
-                cn.query("SELECT * FROM security WHERE user_id = ?", [uid], (err, rows) => {
-                    
+                sql = "SELECT * FROM security WHERE user_id = ?";
+                cn.query(sql, [uid], (err, rows) => {
                     cn.release();
                     
                     require("../../lib/security/initiate")(uid, req, rows[0], security => {
@@ -60,7 +88,10 @@ module.exports = function(req, res) {
                             // User has no extra security measures
                             // Login user
                             require("../../lib/login/doLogin")(req, uid);
-                            res.json({error: false, loggedIn: true, redirect: req.session.redirect ? req.session.redirect : "" });
+                            res.json({
+                                error: false, loggedIn: true, redirect:
+                                    req.session.redirect ? req.session.redirect : ""
+                            });
                         }
                         else {
                             if (security.error) {
