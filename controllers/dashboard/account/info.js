44d9a398a061783f27186c0097019b719e95868e
MrXyfir <mr@xyfir.com>
2016-09-06 13:34:11 -0700

    Output loggedIn property

--- controllers/dashboard/account/info.js
+++ controllers/dashboard/account/info.js
@@ -3,16 +3,28 @@ const db = require("../../../lib/db.js");
 /*
     GET api/dashboard/account
     RETURN
-        { email: string, recovered: boolean }
+        { loggedIn: boolean, email?: string, recovered?: boolean }
 */
 module.exports = function(req, res) {
+
+    if (!req.session.uid) {
+        res.json({ loggedIn: false });
+        return;
+    }
     
     db(cn => {
         cn.query("SELECT email FROM users WHERE id = ?", [req.session.uid], (err, rows) => {
             cn.release();
-            res.json({
-                email: rows[0].email, recovered: req.session.recovered
-            });
+            
+            if (err || !rows.length) {
+                res.json({ loggedIn: false });
+            }
+            else {
+                res.json({
+                    email: rows[0].email, recovered: req.session.recovered,
+                    loggedIn: true
+                });
+            }
         });
     });
 
