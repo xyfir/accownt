08a688d6f868c68eab0751897c3cab89e090e0d7
MrXyfir <mr@xyfir.com>
2016-09-24 11:18:01 -0700

    Improve 'create profile' output

--- client/components/dashboard/Profiles.jsx
+++ client/components/dashboard/Profiles.jsx
@@ -19,7 +19,6 @@ export default class Profiles extends React.Component {
 		};
 
 		this._updateProfiles = this._updateProfiles.bind(this);
-		this.onCreateProfile = this.onCreateProfile.bind(this);
 	}
 	
 	componentWillMount() {
@@ -27,34 +26,29 @@ export default class Profiles extends React.Component {
 	}
 	
 	onCreateProfile() {
-		if (this.state.creatingProfile) {
-			const data = {
-				name: this.refs.name.value,
-				email: this.refs.email.value,
-				fname: this.refs.fname.value,
-				lname: this.refs.lname.value,
-				gender: this.refs.gender.value,
-				phone: this.refs.phone.value,
-				birthdate: this.refs.birthdate.value,
-				address: this.refs.address.value,
-				zip: this.refs.zip.value,
-				region: this.refs.region.value,
-				country: this.refs.country.value
-			};
-			
-			request({
-				url: "../api/dashboard/profiles/",
-				method: "POST", data,
-				success: (result) => {
-					this.setState(result);
-					this.setState({creatingProfile: false});
-					this._updateProfiles();
-				}
-			});
-		}
-		else {
-			this.setState({creatingProfile: true});
-		}
+		const data = {
+			name: this.refs.name.value,
+			email: this.refs.email.value,
+			fname: this.refs.fname.value,
+			lname: this.refs.lname.value,
+			gender: this.refs.gender.value,
+			phone: this.refs.phone.value,
+			birthdate: this.refs.birthdate.value,
+			address: this.refs.address.value,
+			zip: this.refs.zip.value,
+			region: this.refs.region.value,
+			country: this.refs.country.value
+		};
+		
+		request({
+			url: "../api/dashboard/profiles/",
+			method: "POST", data,
+			success: (result) => {
+				this.setState(result);
+				this.setState({creatingProfile: false});
+				this._updateProfiles();
+			}
+		});
 	}
 
 	_updateProfiles() {
@@ -71,58 +65,20 @@ export default class Profiles extends React.Component {
 			userAlert = <Alert type="error" title="Error!">{this.state.message}</Alert>;
 		else if (this.state.message)
 			userAlert = <Alert type="success" title="Success!">{this.state.message}</Alert>;
-		
-		let createProfile;
-		
-		if (this.state.creatingProfile) {
-			createProfile = (
-				<div className="profile-create">
-					<h2>Create a Profile</h2>
-					<p>All fields other than profile name are optional.</p>
-					<hr />
-
-					<input type="text" placeholder="Profile Name" ref="name" />
-					<input type="email" placeholder="Email" ref="email" />
-					
-					<br />
-
-					<input type="text" placeholder="First Name" ref="fname" />
-					<input type="text" placeholder="Last Name" ref="lname" />
-					<select ref="gender">
-						<option value="0">-</option>
-						<option value="1">Male</option>
-						<option value="2">Female</option>
-						<option value="3">Other</option>
-					</select>
-					<input type="tel" placeholder="Phone Number" ref="phone" />
-					<input type="text" placeholder="Birthdate (2020-07-31)" ref="birthdate" />
-					
-					<br />
-					
-					<input type="text" placeholder="Address" ref="address" />
-					<input type="number" placeholder="Zip" ref="zip" />
-					<input type="text" placeholder="Region/State/Province" ref="region" />
-					<input type="text" placeholder="Country (US/CA/UK/etc)" ref="country" />
-				</div>
-			);
-		}
 	
 		return (
-			<div className="dashboard-body col-sm-12 col-md-8">
+			<div className="dashboard-body dashboard-profiles">
 				{userAlert}
 				
-				<h2>Profiles</h2>
 				<p>
 					Profiles allow you to easily give services linked to your Xyfir Account access to required or optional information. 
 					When linking a service to your Xyfir Account you will be able to choose a profile to for the service to access.
 				</p>
-				<hr />
 			
 				<div className="profile-list">{
 					this.state.profiles.map(profile => {
 						return (
 							<Profile
-								picture={profile.picture}
 								name={profile.name}
 								id={profile.profile_id}
 								update={this._updateProfiles}
@@ -131,8 +87,67 @@ export default class Profiles extends React.Component {
 					})
 				}</div>
 				
-				{createProfile}
-				<Button onClick={this.onCreateProfile}>Create Profile</Button>
+				{this.state.creatingProfile ? (
+					<div className="modal profile-create">
+						<a
+							onClick={() => this.setState({ creatingProfile: false })}
+							className="icon-close"
+						/>
+
+						<h2>Create a Profile</h2>
+						<p>All fields other than profile name are optional.</p>
+
+						<label>Profile Name</label>
+						<input type="text" ref="name" />
+						
+						<label>Email</label>
+						<input type="email" ref="email" />
+						
+						<br />
+
+						<label>First Name</label>
+						<input type="text" ref="fname" />
+						
+						<label>Last Name</label>
+						<input type="text" placeholder="Last Name" ref="lname" />
+						
+						<label>Gender</label>
+						<select ref="gender">
+							<option value="0">-</option>
+							<option value="1">Male</option>
+							<option value="2">Female</option>
+							<option value="3">Other</option>
+						</select>
+
+						<label>Phone #</label>
+						<input type="tel" ref="phone" />
+						
+						<label>Birthdate</label>
+						<input type="text" ref="birthdate" />
+						
+						<br />
+						
+						<label>Address</label>
+						<input type="text" ref="address" />
+						
+						<label>Zip Code</label>
+						<input type="number" ref="zip" />
+						
+						<label>State/Province/Region Code</label>
+						<input type="text" ref="region" />
+						
+						<label>Country Code</label>
+						<input type="text" ref="country" />
+
+						<Button onClick={() => this.onCreateProfile()}>Create Profile</Button>
+					</div>
+				) : (
+					<div />
+				)}
+
+				<Button onClick={() => this.setState({ creatingProfile: true })}>
+					Create Profile
+				</Button>
 			</div>
 		);
 	}
