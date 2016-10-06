5a1cd477b311e5fc88c08f686f4963d0d2c3f8fa
MrXyfir <mr@xyfir.com>
2016-10-06 15:09:06 -0700

    Change input type to number

--- client/components/login/SmsVerifyStep.jsx
+++ client/components/login/SmsVerifyStep.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default class SmsVerifyStep extends React.Component {
 
 	render() {
-		return <input type="text"  id="smsCode" placeholder="SMS Code" />;
+		return <input type="number"  id="smsCode" placeholder="SMS Code" />;
 	}
 
 }
