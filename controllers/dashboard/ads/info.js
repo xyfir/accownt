b189dac3a29dca0a74ccef08c6b3132e0cccceef
MrXyfir <mr@xyfir.com>
2016-10-07 13:58:39 -0700

    Clean up code

--- controllers/dashboard/ads/info.js
+++ controllers/dashboard/ads/info.js
@@ -1,33 +1,36 @@
 const request = require("request");
-const config = require("../../../config");
-const db = require("../../../lib/db");
+const db = require("lib/db");
+
+const config = require("config");
 	
 /*
     GET api/dashboard/ads
     RETURN
-        {info: string/json-string}
+        { info: string|json-string }
 */
 module.exports = function(req, res) {
+
+    let sql = `
+        SELECT xad_id FROM users WHERE id = ?
+    `;
     
-    db(cn => {
-        cn.query("SELECT xad_id FROM users WHERE id = ?", [req.session.uid], (err, rows) => {
-            cn.release();
-            
-            if (err || !rows.length) {
-                res.json({info: ""});
-                return;
-            }
-            
-            request({
-                url: config.addresses.xads + "api/xad-id/" + req.session.uid + "/" + rows[0].xad_id
-                    + "?secret=" + config.keys.xadid,
-            }, (err, response, body) => {
-                if (err)
-                    res.json({info: ""});
-                else
-                    res.json({info: JSON.parse(body).info});
-            });
+    db(cn => cn.query(sql, [req.session.uid], (err, rows) => {
+        cn.release();
+        
+        if (err || !rows.length) {
+            res.json({ info: "" });
+            return;
+        }
+        
+        request({
+            url: config.addresses.xads + "api/xad-id/" + req.session.uid + "/" + rows[0].xad_id
+                + "?secret=" + config.keys.xadid,
+        }, (err, response, body) => {
+            if (err)
+                res.json({ info: "" });
+            else
+                res.json({ info: JSON.parse(body).info });
         });
-    });
+    }));
 
 }
