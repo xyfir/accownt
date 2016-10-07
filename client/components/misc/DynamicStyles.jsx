5832a5900429d29e1c1b55e95dfd31dd681aaff9
MrXyfir <mr@xyfir.com>
2016-10-07 14:52:43 -0700

    Generate dynamic CSS styles

---
+++ client/components/misc/DynamicStyles.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default class DynamicStyles extends React.Component {
+	
+    constructor(props) {
+        super(props);
+    }
+
+    _generateStyles() {
+        return `
+            #content {
+                margin-top: ${
+                    document.querySelector("header nav").offsetHeight
+                }px;
+                margin-bottom: ${
+                    document.querySelector("footer").offsetHeight
+                }px;
+            }
+        `;
+    }
+
+	render() {
+		return <style>{this._generateStyles()}</style>;
+	}
+
+}
