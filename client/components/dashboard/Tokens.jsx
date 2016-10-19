13ee49fec05ce4a8dd7bd9ed4c54ad95bd849f5f
MrXyfir <mr@xyfir.com>
2016-10-19 16:34:53 -0700

    Explain access tokens

--- client/components/dashboard/Tokens.jsx
+++ client/components/dashboard/Tokens.jsx
@@ -54,41 +54,52 @@ export default class AccessTokens extends React.Component {
         if (this.state.loading) return <div />;
 
 		return (
-			<div className="dashboard-body dashboard-tokens">{
-                this.state.tokens.map((token, i) => {
-                    return (
-                        <div className="token">
-                            <dl>
-                                <dt>Token</dt>
-                                <dd className="token-short">{token.token.substr(1, 10)}...</dd>
+			<section className="dashboard-body dashboard-tokens">
+                <p>
+                    Access tokens are generated when you login to a service via Xyfir Accounts.
+                    <br />
+                    Access tokens allow previously authorized devices to access services that use Xyfir Accounts without having to login for each session.
+                    <br />
+                    It is recommended to delete any tokens you don't recognize or if one of your devices are stolen or compromised. Deleting a token only means that the device which has stored that token will have to login to the corresponding service before it can access your account.
+                </p>
+                <div class="tokens">{
+                    this.state.tokens.map((token, i) => {
+                        return (
+                            <div className="token">
+                                <dl>
+                                    <dt>Token</dt>
+                                    <dd className="token-short">{
+                                        token.token.substr(1, 10)
+                                    }...</dd>
 
-                                <dt>Service Name</dt>
-                                <dd>{
-                                    this.state.services.find(
-                                        s => s.id == token.service_id
-                                    ).name
-                                }</dd>
-                            
-                                <dt>Created</dt>
-                                <dd>{(new Date(token.created)).toLocaleString()}</dd>
+                                    <dt>Service Name</dt>
+                                    <dd>{
+                                        this.state.services.find(
+                                            s => s.id == token.service_id
+                                        ).name
+                                    }</dd>
+                                
+                                    <dt>Created</dt>
+                                    <dd>{(new Date(token.created)).toLocaleString()}</dd>
 
-                                <dt>Last Used</dt>
-                                <dd>{(new Date(token.last_use)).toLocaleString()}</dd>
+                                    <dt>Last Used</dt>
+                                    <dd>{(new Date(token.last_use)).toLocaleString()}</dd>
 
-                                <dt>Expires</dt>
-                                <dd>{(new Date(token.expires)).toLocaleString()}</dd>
-                            </dl>
+                                    <dt>Expires</dt>
+                                    <dd>{(new Date(token.expires)).toLocaleString()}</dd>
+                                </dl>
 
-                            <button
-                                className="btn-danger"
-                                onClick={() => this.onDelete(i)}
-                            >
-                                <span className="icon-delete" />Delete Token
-                            </button>
-                        </div>
-                    );
-                })
-            }</div>
+                                <button
+                                    className="btn-danger"
+                                    onClick={() => this.onDelete(i)}
+                                >
+                                    <span className="icon-delete" />Delete Token
+                                </button>
+                            </div>
+                        );
+                    })
+                }</div>
+            }</section>
 		);
 	}
 	
