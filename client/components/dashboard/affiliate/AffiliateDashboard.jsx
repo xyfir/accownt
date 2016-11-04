b0f764ca7b569c54d8ec723e93486b4f5ecb8c24
MrXyfir <mr@xyfir.com>
2016-11-04 13:59:56 -0700

    Add AffiliateDashboard component

---
+++ client/components/dashboard/affiliate/AffiliateDashboard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+// Components
+
+// Modules
+import request from "lib/request";
+
+export default class AffiliateDashboard extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+		request({
+			url: "../api/dashboard/account",
+			success: (result) => {
+				if (!result.loggedIn)
+                    location.hash = "/login";
+                else if (!result.affiliate)
+                    locoation.hash = "/dashboard/user";
+			}
+		});
+	}
+
+    render() {
+        return (
+            <div className="dashboard-affiliate">
+                
+			</div>
+        );
+    }
+
+}
