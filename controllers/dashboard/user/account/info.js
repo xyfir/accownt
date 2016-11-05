381d2c2131ee1ca7977b7be5251adb8a135e8bb3
MrXyfir <mr@xyfir.com>
2016-11-04 17:04:28 -0700

    Return affiliate boolean

--- controllers/dashboard/user/account/info.js
+++ controllers/dashboard/user/account/info.js
@@ -3,7 +3,10 @@ const db = require("lib/db");
 /*
     GET api/dashboard/user/account
     RETURN
-        { loggedIn: boolean, email?: string, recovered?: boolean }
+        {
+            loggedIn: boolean,
+            email?: string, recovered?: boolean, affiliate?: boolean
+        }
 */
 module.exports = function(req, res) {
 
@@ -11,21 +14,23 @@ module.exports = function(req, res) {
         res.json({ loggedIn: false });
         return;
     }
+
+    let sql = `
+        SELECT email, affiliate FROM users WHERE id = ?
+    `;
     
-    db(cn => {
-        cn.query("SELECT email FROM users WHERE id = ?", [req.session.uid], (err, rows) => {
-            cn.release();
-            
-            if (err || !rows.length) {
-                res.json({ loggedIn: false });
-            }
-            else {
-                res.json({
-                    email: rows[0].email, recovered: req.session.recovered,
-                    loggedIn: true
-                });
-            }
-        });
-    });
+    db(cn => cn.query(sql, [req.session.uid], (err, rows) => {
+        cn.release();
+        
+        if (err || !rows.length) {
+            res.json({ loggedIn: false });
+        }
+        else {
+            res.json({
+                email: rows[0].email, recovered: req.session.recovered,
+                loggedIn: true, affiliate: rows[0].affiliate
+            });
+        }
+    }));
 
 }
