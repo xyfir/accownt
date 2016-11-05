40e4da102ee170becf9a09a4341bae4e81b88142
MrXyfir <mr@xyfir.com>
2016-11-05 16:56:36 -0700

    Add POST api/dashboard/affiliate controller

---
+++ controllers/dashboard/affiliate/create-campaign.js
@@ -0,0 +1,71 @@
+const db = require("lib/db");
+
+/*
+    POST api/dashboard/affiliate
+    REQUIRED
+        code: string, promo: number
+    RETURN
+        { error: boolean, message: string }
+    DESCRIPTION
+        Create an affiliate campaign for an affiliate promotion
+*/
+module.exports = function(req, res) {
+
+    let sql = `
+        SELECT (
+            SELECT COUNT(*) FROM affiliate_campaigns WHERE code = ?
+        ) as code_exists, (
+            SELECT COUNT(*) FROM users WHERE id = ? AND affiliate = 1
+        ) as is_affiliate, (
+            SELECT COUNT(*) FROM affiliate_campaigns
+            WHERE user_id = ? AND promo_id = ?
+        ) as campaign_exists, (
+            SELECT COUNT(*) FROM affiliate_promotions WHERE id = ?
+        ) as promo_exists
+    `, vars = [
+        req.body.code,
+        req.session.uid,
+        req.session.uid, req.body.promo,
+        req.body.promo
+    ];
+    
+    db(cn => cn.query(sql, vars, (err, rows) => {
+        let error = "";
+        
+        if (err)
+            error = "An unknown error occured";
+        else if (rows[0].code_exists)
+            error = "Promo code already in use";
+        else if (!rows[0].is_affiliate)
+            error = "You are not an affiliate";
+        else if (rows[0].campaign_exists)
+            error = "You already have a campaign with this promotion";
+        else if (!rows[0].promo_exists)
+            error = "Invalid promotion";
+        else if (!/^[A-Z0-9]{4,10}$/.test(req.body.code))
+            error = "Invalid code length / characters";
+        
+        if (error) {
+            cn.release();
+            res.json({ error: true, message: error });
+        }
+        else {
+            let sql = `
+                INSERT INTO affiliate_campaigns (user_id, promo_id, code)
+                VALUES (?, ?, ?)
+            `, vars = [
+                req.session.uid, req.body.promo, req.body.code
+            ];
+
+            cn.query(sql, vars, (err, result) => {
+                cn.release();
+
+                if (err || !result.affectedRows)
+                    res.json({ error: true, message: "An unknown error occured" });
+                else
+                    res.json({ error: false, message: "Campaign created" });
+            });
+        }
+    }));
+
+}
