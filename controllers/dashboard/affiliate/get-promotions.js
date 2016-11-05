bff20097f65c841406ef1e16ebf52ef993e32022
MrXyfir <mr@xyfir.com>
2016-11-05 14:30:47 -0700

    Add GET api/dashboard/affiliate/promotions controller

---
+++ controllers/dashboard/affiliate/get-promotions.js
@@ -0,0 +1,27 @@
+const db = require("lib/db");
+
+/*
+    GET api/dashboard/affiliate/promotions
+    RETURN
+        { promotions: [{
+            id: number, name: string, description: string
+        }] }
+    DESCRIPTION
+        Return all affiliate promotions
+*/
+module.exports = function(req, res) {
+
+    let sql = `
+        SELECT id, name, description FROM affiliate_promotions
+    `;
+    
+    db(cn => cn.query(sql, (err, rows) => {
+        cn.release();
+
+        if (err || !rows.length)
+            res.json({ promotions: [] });
+        else
+            res.json({ promotions: rows });
+    }));
+
+}
