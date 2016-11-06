938f5b1b8b38a9754cd742dbe222cbb3a438a62b
MrXyfir <mr@xyfir.com>
2016-11-06 14:29:57 -0800

    Add POST api/affiliate/purchase controller

---
+++ controllers/affiliate/purchase.js
@@ -0,0 +1,65 @@
+const db = require("lib/db");
+
+/*
+    POST api/affiliate/purchase
+    REQUIRED
+        {
+            service: number, serviceKey: string, promoCode: string,
+            amount: number
+        }
+    RETURN
+        { error: boolean, message?: string }
+    DESCRIPTION
+        Increments an affiliate campaign's purchases and earnings
+*/
+module.exports = function(req, res) {
+
+    let sql = `
+        SELECT (
+            SELECT COUNT(*) FROM affiliate_promotions
+            WHERE service_id = ? AND id IN (
+                SELECT promo_id FROM affiliate_campaigns WHERE code = ?
+            )
+        ) as valid_code, (
+            SELECT COUNT(*) FROM services WHERE id IN (
+                SELECT service_id FROM service_keys
+                WHERE service_id = ? AND service_key = ?
+            )
+        ) as valid_service
+    `, vars = [
+        req.body.service,
+        req.body.promoCode,
+        req.body.service, req.body.serviceKey
+    ];
+    
+    db(cn => cn.query(sql, vars, (err, rows) => {
+        let error = "";
+
+        if (err)
+            error = "An unknown error occured";
+        else if (!rows[0].valid_code)
+            error = "Invalid promotion";
+        else if (!rows[0].valid_service)
+            error = "Invalid service / key";
+
+        if (error) {
+            cn.release();
+            res.json({ error: true, message: error });
+        }
+        else {
+            res.json({ error: false });
+
+            sql = `
+                UPDATE affiliate_campaigns SET
+                    purchases = purchases + 1, earnings = earnings + ?
+                WHERE code = ?
+            `, vars = [
+                (+req.body.amount * 0.10),
+                req.body.promoCode
+            ];
+
+            cn.query(sql, vars, (err, result) => cn.release());
+        }
+    }));
+
+}
