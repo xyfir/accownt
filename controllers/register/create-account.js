4e45a9e454c5357b4bc707956d3ea426b0eaf74c
MrXyfir <mr@xyfir.com>
2016-11-13 13:43:54 -0800

    Update and improve code

--- controllers/register/create-account.js
+++ controllers/register/create-account.js
@@ -1,7 +1,8 @@
+const sendVerificationEmail = require("lib/email/send-verification");
 const request = require("request");
-const config = require("../../config");
+const config = require("config");
 const bcrypt = require("bcrypt");
-const db = require("../../lib/db"); 
+const db = require("lib/db"); 
 
 /*
     POST api/register
@@ -14,11 +15,11 @@ module.exports = function(req, res) {
 
     // Check for required fields
     if (!req.body.email || !req.body.password || !req.body.recaptcha) {
-        res.json({error: true, message: "Required field(s) empty."});
+        res.json({ error: true, message: "Required field(s) empty." });
         return;
     }
 
-    // Check if reCaptcha response is valid
+    // Check if recaptcha response is valid
     request.post({
         url: "https://www.google.com/recaptcha/api/siteverify",
         form: {
@@ -29,78 +30,97 @@ module.exports = function(req, res) {
     }, (e, r, body) => {
         if (e || !JSON.parse(body).success) {
             res.json({ error: true, message: "Invalid captcha" });
+            return;
         }
-        else {
-            // Attempt to register user
-            db(cn => {
-                let sql = "SELECT id FROM users WHERE email = ? AND verified = ?"; 
-                cn.query(sql, [req.body.email, 1], (err, rows) => {
-                    
-                    // Email already linked to account
-                    if (rows.length > 0) {
+        
+        let sql = `
+            SELECT id FROM users WHERE email = ? AND verified = ?
+        `, vars = [
+            req.body.email, 1
+        ];
+
+        // Attempt to register user
+        db(cn => cn.query(sql, vars, (err, rows) => {
+            // Email already linked to account
+            if (rows.length > 0) {
+                cn.release();
+                res.json({
+                    error: true, message: "Email is already linked to an account."
+                }); return;
+            }
+            
+            // Hash password with bcrypt + random salt
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                // Create user's account
+                sql = "INSERT INTO users SET ?";
+                let data = {
+                    email: req.body.email,
+                    password: hash,
+                    verified: 0
+                };
+                
+                cn.query(sql, data, (err, result) => {
+                    if (err || !result.insertId) {
                         cn.release();
                         res.json({
-                            error: true, message: "Email is already linked to an account."
+                            error: true, message: "Unknown error occured"
                         }); return;
                     }
-                    
-                    // Hash password with bcrypt + random salt
-                    bcrypt.hash(req.body.password, 10, (err, hash) => {
+
+                    const uid = result.insertId;
+
+                    // Generate XADID from Xyfir Ads
+                    request.post({
+                        url: config.addresses.xads + "api/xad-id/" + uid,
+                        form: { secret: config.keys.xadid }
+                    }, (err, response, body) => {
+                        let error = false;
                         
-                        let data = {
-                            email: req.body.email,
-                            password: hash,
-                            verified: 0
-                        };
+                        if (err) {
+                            error = true;
+                        }
+                        else {
+                            body = JSON.parse(body);
+                            if (body.error) error = true;
+                        }
                         
-                        // Create user's account
-                        sql = "INSERT INTO users SET ?";
-                        cn.query(sql, data, (err, result) => {
-                            // Generate XADID from Xyfir Ads
-                            request.post({
-                                url: config.addresses.xads + "api/xad-id/" + result.insertId,
-                                form: { secret: config.keys.xadid }
-                            }, (err, response, body) => {
-                                let error = false;
-                                
-                                if (err) {
-                                    error = true;
-                                }
-                                else {
-                                    body = JSON.parse(body);
-                                    if (body.error) error = true;
-                                }
-                                
-                                // Delete user and respond with error
-                                if (error) {
-                                    sql = "DELETE FROM users WHERE id = ?";
-                                    cn.query(sql, [result.insertId], (err, result) => {
-                                        cn.release();
-                                        res.json({error: true, message: "Unknown error occured"});
-                                    });
-                                }
-                                // Set xadid
-                                else {
-                                    sql = "UPDATE users SET xad_id = ? WHERE id = ?"; 
-                                    cn.query(sql, [body.xadid, result.insertId], (err, result) => {
-                                        // Send email verification email
-                                        require("../../lib/email/sendVerification")(result.insertId, req.body.email);
-                                        
-                                        res.json({error: false, message: ""});
-                                        
-                                        // Create row in security table with user"s id
-                                        const data = {
-                                            user_id: result.insertId,
-                                        };
-                                        cn.query("INSERT INTO security SET ?", data, () => cn.release());
-                                    });
-                                }
+                        // Delete user and respond with error
+                        if (error) {
+                            sql = "DELETE FROM users WHERE id = ?";
+                            cn.query(sql, [uid], (err, result) => {
+                                cn.release();
+                                res.json({
+                                    error: true, message: "Unknown error occured"
+                                });
                             });
-                        });
+                        }
+                        // Set xadid
+                        else {
+                            sql = `
+                                UPDATE users SET xad_id = ? WHERE id = ?
+                            `, vars = [
+                                body.xadid, uid
+                            ];
+
+                            cn.query(sql, vars, (err, result) => {
+                                res.json({ error: false, message: "" });
+
+                                // Create row in security table with user's id
+                                cn.query("INSERT INTO security SET ?", {
+                                    user_id: uid
+                                }, (err, result) => {
+                                    cn.release();
+
+                                    // Send email verification email
+                                    // !! Requires row in security table
+                                    sendVerificationEmail(uid, req.body.email);
+                                });
+                            });
+                        }
                     });
                 });
             });
-        }
+        }));
     });
 
 }
