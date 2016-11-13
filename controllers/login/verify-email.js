65cfb47c3f5c6ddbe42f2e74e30fa9b050cca9f4
MrXyfir <mr@xyfir.com>
2016-11-13 13:43:29 -0800

    On success delete unverified accounts with same email

--- controllers/login/verify-email.js
+++ controllers/login/verify-email.js
@@ -7,21 +7,51 @@ const db = require("lib/db");
         Verify a users email if :uid/:auth are valid
 */
 module.exports = function(req, res) {
-
+    
     validateToken({
         user: req.params.uid, token: req.params.auth
     }, isValid => {
+        
         if (isValid) {
-            require("lib/login/doLogin")(req, req.params.uid);
+            req.session.uid = req.params.uid;
+
+            let sql = `
+                UPDATE users SET verified = ? WHERE id = ?
+            `, vars = [
+                1, req.params.uid
+            ];
             
             // Set verified
-            db(cn => {
-                cn.query("UPDATE users SET verified = ? WHERE id = ?", [1, req.params.uid], (err, result) => {
+            db(cn => cn.query(sql, vars, (err, result) => {
+                if (err || !result.affectedRows) {
                     cn.release();
-                    
-                    res.redirect(req.session.redirect ? req.session.redirect : "/app/#/dashboard");
-                });
-            });
+                    res.redirect("/app/#/login");
+                }
+                else {
+                    res.redirect(
+                        req.session.redirect
+                        ? req.session.redirect : "/app/#/dashboard/user"
+                    );
+                    req.session.redirect = "";
+
+                    // Get user's email
+                    sql = `
+                        SELECT email FROM users WHERE id = ?
+                    `, vars = [
+                        req.session.uid
+                    ];
+
+                    cn.query(sql, vars, (err, rows) => {
+                        // Delete unverified accounts with the same email
+                        sql = `
+                            DELETE FROM users WHERE email = ? AND verified = 0 
+                        `, vars = [
+                            rows[0].email
+                        ];
+                        cn.query(sql, (err, result) => cn.release());
+                    });
+                }
+            }));
         }
         else {
             res.redirect("/app/#/login");
