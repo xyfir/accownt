8e252cef4995db2308ff50802fe2bcd81c9e967d
MrXyfir <mr@xyfir.com>
2016-11-16 15:52:53 -0800

    Clean up code. Only return message on error

--- controllers/service/info.js
+++ controllers/service/info.js
@@ -1,11 +1,11 @@
-const db = require("../../lib/db");
+const db = require("lib/db");
 
-const config = require("../../config");
+const config = require("config");
 
 /*
 	GET api/service/:service
 	RETURNED
-		{ error: bool, message: string, service?: {}, profiles?: [] }
+		{ error: bool, message?: string, service?: {}, profiles?: [] }
 	DESCRIPTION
 		Returns to user when linking service to account
 */
@@ -17,40 +17,58 @@ module.exports = function(req, res) {
 		res.json(({ error: true, message: "Not logged in" }));
 		return;
 	}
+
+	// Get service's info
+	let sql = `
+		SELECT name, description, info FROM services WHERE id = ?
+	`, vars = [
+		req.params.service
+	];
 	
-    db(cn => {
-		cn.query("SELECT name, description, info FROM services WHERE id = ?", [req.params.service], (err, rows) => {
-			
-			if (err || rows.length == 0) {
+    db(cn => cn.query(sql, vars, (err, rows) => {
+		if (err || rows.length == 0) {
+			cn.release();
+			res.json({ error: true, message: "Service does not exist." });
+			return;
+		}
+		
+		const data = {
+			name: rows[0].name,
+			description: rows[0].description,
+			requested: JSON.parse(rows[0].info)
+		};
+
+		// Check if user is already linked to service
+		sql = `
+			SELECT xyfir_id FROM linked_services
+			WHERE user_id = ? AND service_id = ?
+		`, vars = [
+			req.session.uid, req.params.service
+		];
+		
+		cn.query(sql, vars, (err, rows) => {
+			if (rows.length > 0) {
 				cn.release();
-				res.json({error: true, message: "Service does not exist."});
-				return;
+				res.json({
+					error: true, message: "Service is already linked to account."
+				}); return;
 			}
-			
-			const data = {
-				name: rows[0].name,
-				description: rows[0].description,
-				requested: JSON.parse(rows[0].info)
-			};
-			
-			// Check if user is already linked to service
-			cn.query("SELECT xyfir_id FROM linked_services WHERE user_id = ? AND service_id = ?",
-			[req.session.uid, req.params.service], (err, rows) => {
-				if (rows.length > 0) {
-					cn.release();
-					res.json({ error: true, message: "Service is already linked to account." });
-					return;
-				}
+
+			// Grab user's profiles			
+			sql = `
+				SELECT profile_id, name FROM profiles WHERE user_id = ?
+			`, vars = [
+				req.session.uid
+			];
+
+			cn.query(sql, vars, (err, rows) => {
+				cn.release();
 				
-				// Grab user"s profiles
-				cn.query("SELECT profile_id, name FROM profiles WHERE user_id = ?", [req.session.uid], (err, rows) => {
-					cn.release();
-					
-					// profiles: [{name,profile_id},{...}]
-					res.json({ error: false, message: "", service: data, profiles: rows });
+				res.json({
+					error: false, service: data, profiles: rows
 				});
-			})
-		});
-	});
+			});
+		})
+	}));
 
 }
