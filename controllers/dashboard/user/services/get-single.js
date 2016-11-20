257038bb8ecfb48485732a700646a6578b0881ff
MrXyfir <mr@xyfir.com>
2016-11-20 13:44:52 -0800

    Add service address to return. Clean up code.

--- controllers/dashboard/user/services/get-single.js
+++ controllers/dashboard/user/services/get-single.js
@@ -4,44 +4,58 @@ const db = require("lib/db");
     GET api/dashboard/user/services/:service
     RETURN
         {
-			error: bool, message?: string,
-			service?: { name: string, description: string, info }
+			error: bool, message?: string, service?: {
+				name: string, description: string, address: string, info: {
+					requested: {}, provided: {}
+				}
+			}
 		}
+	DESCRIPTION
+		Return a service's info and user-provided data to the service
 */
 module.exports = function(req, res) {
+
+	let sql = `
+		SELECT name, description, address, info FROM services WHERE id = ?
+	`, vars = [
+		req.params.service
+	];
 	
-    db(cn => {
-		cn.query("SELECT name, description, info FROM services WHERE id = ?", [req.params.service], (err, rows) => {
+    db(cn => cn.query(sql, vars, (err, rows) => {
+		if (err || rows.length == 0) {
+			res.json({ error: true });
+			return;
+		}
+		
+		let data = {
+			name: rows[0].name,
+			address: rows[0].address,
+			description: rows[0].description,
+			info: {
+				requested: JSON.parse(rows[0].info),
+				provided: {}
+			}
+		};
+		
+		sql = `
+			SELECT info FROM linked_services
+			WHERE user_id = ? AND service_id = ?
+		`, vars = [
+			req.session.uid, req.params.service
+		];
+
+		cn.query(sql, vars, (err, rows) => {
+			cn.release();
+			
 			if (err || rows.length == 0) {
-				res.json({error: true});
+				res.json({ error: true });
 				return;
 			}
 			
-			let data = {
-				name: rows[0].name,
-				description: rows[0].description,
-				info: {
-					requested: JSON.parse(rows[0].info)
-				}
-			};
+			data.info.provided = JSON.parse(rows[0].info);
 			
-			cn.query(
-				"SELECT info FROM linked_services WHERE user_id = ? AND service_id = ?",
-				[req.session.uid, req.params.service],
-				(err, rows) => {
-					cn.release();
-					
-					if (err || rows.length == 0) {
-						res.json({error: true});
-						return;
-					}
-					
-					data.info.provided = JSON.parse(rows[0].info);
-					
-					res.json({error: false, message: "", service: data});
-				}
-			);
+			res.json({ error: false, service: data });
 		});
-	});
+	}));
 
 }
