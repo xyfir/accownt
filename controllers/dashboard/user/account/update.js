e04ce714aec3d393de286ddedc3031f8e3110fa6
MrXyfir <mr@xyfir.com>
2017-04-09 18:07:02 -0700

    Improvements
    - Change code style
    - Clean up code
    - Use lib/mysql
    - Use async/await and promises

--- controllers/dashboard/user/account/update.js
+++ controllers/dashboard/user/account/update.js
@@ -1,36 +1,58 @@
-const bcrypt = require("bcrypt");
-const db = require("lib/db");
+const bcrypt = require('bcrypt');
+const mysql = require('lib/mysql');
 
 /*
-    PUT api/dashboard/user/account
-    REQUIRED
-        currentPassword: string, newPassword: string
-    RETURN
-        { error: boolean, message: string }
+  PUT api/dashboard/user/account
+  REQUIRED
+    currentPassword: string, newPassword: string
+  RETURN
+    { error: boolean, message: string }
 */
-module.exports = function(req, res) {
-    
-    db(cn => {
-        cn.query("SELECT password FROM users WHERE id = ?", [req.session.uid], (err, rows) => {
-            // Check if current password matches
-            bcrypt.compare(req.body.currentPassword, rows[0].password, (err, match) => {
-                
-                if (match || req.session.recovered) {
-                    bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
-                        // Update password
-                        cn.query("UPDATE users SET password = ? WHERE id = ?", [hash, req.session.uid]);
-                        res.json({error: false, message: "Password successfully updated"});
-                    });
-                    
-                    req.session.recovered = false;
-                }
-                else {
-                    res.json({error: true, message: "Incorrect password"});
-                }
-                
-                cn.release();
+module.exports = async function(req, res) {
+
+  const { currentPassword, newPassword } = req.body;
+  const db = new mysql();
+
+  try {
+    await db.getConnection();
+
+    const rows = await db.query(
+      'SELECT password FROM users WHERE id = ?',
+      [req.session.uid]
+    );
+
+    // Check if current password matches
+    bcrypt.compare(currentPassword, rows[0].password, (err, match) => {
+      if (match || req.session.recovered) {
+        bcrypt.hash(newPassword, 10, (err, hash) => {
+          // Update password
+          db.query(
+            'UPDATE users SET password = ? WHERE id = ?',
+            [hash, req.session.uid]
+          )
+          .then(result => {
+            db.release();
+            res.json({
+              error: false, message: 'Password successfully updated'
             });
+          })
+          .catch(err => {
+            db.release();
+            res.json({ error: true, message: 'An unknown error occurred' });
+          });
         });
+        
+        req.session.recovered = false;
+      }
+      else {
+        db.release();
+        res.json({ error: true, message: 'Incorrect password' });
+      }
     });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: 'An unknown error occurred' });
+  }
 
 }
