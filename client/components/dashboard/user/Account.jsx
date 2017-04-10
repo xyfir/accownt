58cd4c583a04ff65970d52bb564ae145cac02942
MrXyfir <mr@xyfir.com>
2017-04-09 18:04:59 -0700

    Improvements, changes
    - Change code style
    - Clean up code
    - Use superagent
    - Remove passworld length requirement

--- client/components/dashboard/user/Account.jsx
+++ client/components/dashboard/user/Account.jsx
@@ -1,99 +1,87 @@
-import React from "react";
+import request from 'superagent';
+import React from 'react';
 
 // Components
-import Button from "components/forms/Button";
-
-// Modules
-import request from "lib/request";
+import Button from 'components/forms/Button';
 
 export default class Account extends React.Component {
-	
-	constructor(props) {
-		super(props);
+  
+  constructor(props) {
+    super(props);
 
-		this.state = { email: "", recovered: false };
-	}
-	
-	componentWillMount() {
-		request({
-			url: "../api/dashboard/user/account",
-			success: (result) => {
-				if (result.loggedIn)
-					this.setState(result);
-				else
-					location.hash = "/login";
-			}
-		});
-	}
-	
-	onUpdatePassword(e) {
-		e.preventDefault();
+    this.state = { email: '', recovered: false };
+  }
+  
+  componentWillMount() {
+    request
+      .get('../api/dashboard/user/account')
+      .end((err, res) => {
+        if (res.body.loggedIn)
+          this.setState(res.body);
+        else
+          location.hash = '/login';
+      });
+  }
+  
+  onUpdatePassword(e) {
+    e.preventDefault();
 
-		const curPass = this.refs.cpassword.value + "";
-		const newPass = this.refs.npassword.value;
-		const conPass = this.refs.rpassword.value;
-		
-		if (newPass != conPass) {
-			swal("Error", "Passwords do not match.", "error");
-		}
-		else if (newPass.length < 12) {
-			swal(
-				"Error",
-				"Password needs to be at least 12 characters long.",
-				"error"
-			);
-		}
-		else {
-			request({
-				url: "../api/dashboard/user/account",
-				method: "PUT",
-				data: {
-					currentPassword: curPass,
-					newPassword: newPass
-				},
-				success: (result) => {
-					if (result.error)
-						swal("Error", this.state.message, "error");
-					else if (this.state.message)
-						swal("Success", this.state.message, "success");
-				}
-			});
-		}
-	}
-	
-	render() {
-		return (
-			<div className="dashboard-body dashboard-account">
-				<section className="main">
-					<h3>{this.state.email}</h3>
-					<a href="../api/login/logout" className="link-sm">
-						Logout
-					</a>
-				</section>
-				
-				<section className="change-password">
-					<form onSubmit={(e) => this.onUpdatePassword(e)}>
-						<input
-							type={this.state.recovered ? "hidden" : "password" }
-							ref="cpassword"
-							placeholder="Current Password"
-						/>
-						<input
-							type="password"
-							ref="npassword"
-							placeholder="New Password"
-						/>
-						<input
-							type="password"
-							ref="rpassword"
-							placeholder="Confirm"
-						/>
+    const curPass = this.refs.cpassword.value;
+    const newPass = this.refs.npassword.value;
+    const conPass = this.refs.rpassword.value;
+    
+    if (newPass != conPass) {
+      swal('Error', 'Passwords do not match.', 'error');
+    }
+    else {
+      request
+        .put('../api/dashboard/user/account')
+        .send({
+          currentPassword: curPass,
+          newPassword: newPass
+        })
+        .end((err, res) => {
+          if (res.body.error)
+            swal('Error', res.body.message, 'error');
+          else
+            swal('Success', res.body.message, 'success');
+        })
+    }
+  }
+  
+  render() {
+    return (
+      <div className='dashboard-body dashboard-account'>
+        <section className='main'>
+          <h3>{this.state.email}</h3>
+          <a href='../api/login/logout' className='link-sm'>
+            Logout
+          </a>
+        </section>
+        
+        <section className='change-password'>
+          <form onSubmit={(e) => this.onUpdatePassword(e)}>
+            <input
+              type={this.state.recovered ? 'hidden' : 'password' }
+              ref='cpassword'
+              placeholder='Current Password'
+            />
+            <input
+              type='password'
+              ref='npassword'
+              placeholder='New Password'
+            />
+            <input
+              type='password'
+              ref='rpassword'
+              placeholder='Confirm'
+            />
 
-						<Button>Update Password</Button>
-					</form>
-				</section>
-			</div>
-		);
-	}
-	
+            <Button>Update Password</Button>
+          </form>
+        </section>
+      </div>
+    );
+  }
+  
 }
