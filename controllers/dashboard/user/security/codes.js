894d70462950859a87c06b9910f6fe10de54467f
MrXyfir <mr@xyfir.com>
2017-04-09 18:07:36 -0700

    Improvements, changes
    - Change code style
    - Clean up code
    - Use lib/mysql
    - Use async/await and promises
    - Update rword

--- controllers/dashboard/user/security/codes.js
+++ controllers/dashboard/user/security/codes.js
@@ -1,99 +1,102 @@
-const rword = require("rword");
-const db = require("lib/db");
+const rword = require('rword');
+const mysql = require('lib/mysql');
 
-const config = require("config");
+const config = require('config');
 
 /*
-    PUT api/dashboard/user/security/codes
-    REQUIRED
-        type: number, count: number
-    RETURN
-        { error: bool, codes: string, message: string }
+  PUT api/dashboard/user/security/codes
+  REQUIRED
+    type: number, count: number
+  RETURN
+    { error: bool, codes: string, message: string }
 */
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
 
-    let sql = "", vars = [];
-		
+  const db = new mysql();
+
+  try {
+    await db.getConnection();
+
+    let sql = '', vars = [];
+      
     // Check if user is removing all codes
     if (req.body.count == 0) {
-        sql = `
-            UPDATE security SET codes = '' WHERE user_id = ?
-        `, vars = [
-            req.session.uid
-        ];
+      sql = `
+        UPDATE security SET codes = '' WHERE user_id = ?
+      `,
+      vars = [
+        req.session.uid
+      ];
 
-        db(cn => cn.query(sql, vars, (err, result) => {
-            cn.release();
-            res.json({
-                error: false, codes: "",
-                message: "Security codes removed from account."
-            });
-        }));
-        return;
+      await db.query(sql, vars);
+      db.release();
+      
+      res.json({
+        error: false, codes: '',
+        message: 'Security codes removed from account'
+      });
+      return;
     }
     
     // Check provided data
     if (
-        req.body.type > 3 || req.body.type == 0
-        || req.body.count > 20 || req.body.count < 5
-    ) {
-        res.json({ error: true, message: "Invalid data." });
-        return;
-    }
+      req.body.type > 3 || req.body.type == 0 ||
+      req.body.count > 20 || req.body.count < 5
+    ) throw 'Invalid data';
     
     // req.body.type: 1 = numbers, 2 = words, 3 = both
     let codes = [], words = 0, numbers = 0;
     
     // Calculate number of numbers to generate
     if (req.body.type == 1)
-        numbers = req.body.count;
+      numbers = req.body.count;
     else if (req.body.type == 3)
-        numbers = Math.floor(Math.random() * req.body.count);
+      numbers = Math.floor(Math.random() * req.body.count);
     
     // Calculate number of words to generate
     if (req.body.type == 2)
-        words = req.body.count;
+      words = +req.body.count;
     else if (req.body.type == 3)
-        words = req.body.count - numbers;
-        
+      words = +req.body.count - numbers;
+      
     // Add random numbers to list
     for (let i = 0; i < numbers; i++) {
-        // 1000 - 9999
-        codes.push(Math.floor(Math.random() * (10000 - 1000) + 1000));
+      // 1000 - 9999
+      codes.push(Math.floor(Math.random() * (10000 - 1000) + 1000));
     }
     
     // Generate words
     if (words > 0) {
-        const rWords = rword(words);
-
-        typeof rWords == "string"
-            ? codes.push(rWords)
-            : rWords.forEach(w => codes.push(w));
+      codes = codes.concat(rword.generate(words));
     }
 
     // Shuffle array
     for (let i = codes.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        
-        let temp = codes[i];
-        codes[i] = codes[j];
-        codes[j] = temp;
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = codes[i];
+
+      codes[i] = codes[j];
+      codes[j] = temp;
     }
 
     sql = `
-        UPDATE security SET codes = ? WHERE user_id = ?
-    `, vars = [
-        codes.join(","), req.session.uid
+      UPDATE security SET codes = ? WHERE user_id = ?
+    `,
+    vars = [
+      codes.join(','), req.session.uid
     ];
+
+    await db.query(sql, vars);
+    db.release();
     
-    // Save list to database
-    db(cn => cn.query(sql, vars, (err, result) => {
-        cn.release();
-        
-        res.json({
-            codes: codes.toString(), error: false,
-            message: "Security codes successfully updated."
-        });
-    }));
+    res.json({
+      codes: codes.join(','), error: false,
+      message: 'Security codes successfully updated'
+    });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err });
+  }
 
 }
