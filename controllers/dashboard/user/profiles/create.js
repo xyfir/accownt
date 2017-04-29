f51e82191c80b700c58ddf654a0554b4d99ddaf9
MrXyfir <mr@xyfir.com>
2017-04-29 09:18:43 -0700

    Improvements, changes
    - Change code style
    - Use async / await
    - Use lib/mysql
    - Update profile validation

--- controllers/dashboard/user/profiles/create.js
+++ controllers/dashboard/user/profiles/create.js
@@ -1,31 +1,37 @@
-const isProfileValid = require("lib/profiles/isValid");
-const db = require("lib/db");
+const validateProfile = require('lib/profiles/validate');
+const mysql = require('lib/mysql');
 
 /*
-    POST api/dashboard/user/profiles
-    REQUIRED
-        **
-    RETURN
-        { error: bool, message: string }
+  POST api/dashboard/user/profiles
+  REQUIRED
+    name: string, fname: stirng, lname: string, address: string, zip: string,
+    region: string,  country: string, email: string, phone: string,
+    gender: number, birthdate: string
+  RETURN
+    { error: bool, message: string }
 */
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
+  
+  const profile = req.body, db = new mysql();
+  profile.user_id = req.session.uid;
+  
+  try {
+    validateProfile(profile);
+
+    await db.getConnection();
+
+    const result = await db.query(
+      'INSERT INTO profiles SET ?', profile
+    );
+
+    if (!result.insertId) throw 'Could not create profile';
     
-    let profile = req.body;
-    
-    profile.user_id = req.session.uid;
-    req.body = {};
-    
-    if (!isProfileValid(res, profile)) return;
-    
-    db(cn => {			
-        cn.query("INSERT INTO profiles SET ?", profile, (err, result) => {
-            cn.release();
-            
-            if (err || !result.insertId)
-                res.json({ error: true, message: "An unknown error occured." });
-            else
-                res.json({ error: false, message: "Profile successfully created." });
-        });
-    });
-    
+    db.release();
+    res.json({ error: false, message: 'Profile successfully created.' });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err });
+  }
+  
 }
--- controllers/dashboard/user/profiles/update.js
+++ controllers/dashboard/user/profiles/update.js
@@ -1,42 +1,48 @@
-const isProfileValid = require("lib/profiles/isValid");
-const db = require("lib/db");
+const validateProfile = require('lib/profiles/validate');
+const mysql = require('lib/mysql');
 
 /*
-    PUT api/dashboard/user/profiles/:profile
-    REQUIRED
-        name: string, fname: stirng, lname: string, address: string,
-        zip: string, region: string,  country: string,
-        email: string, phone: string, gender: number,
-        birthdate: string
-    RETURN
-        { error: bool, message: string }
+  PUT api/dashboard/user/profiles/:profile
+  REQUIRED
+    name: string, fname: stirng, lname: string, address: string,
+    zip: string, region: string,  country: string,
+    email: string, phone: string, gender: number,
+    birthdate: string
+  RETURN
+    { error: bool, message: string }
 */
-module.exports = function(req, res) {
-		
-    let profile = req.body;
+module.exports = async function(req, res) {
+  
+  const profile = req.body, db = new mysql();
+  
+  try {
+    validateProfile(profile);
+
+    const sql = `
+      UPDATE profiles SET
+        name = ?, fname = ?, lname = ?, address = ?, zip = ?, region = ?,
+        country = ?, email = ?, phone = ?, gender = ?, birthdate = ?,
+        picture = ?
+      WHERE user_id = ? AND profile_id = ?
+    `,
+    vars = [
+      profile.name, profile.fname, profile.lname, profile.address, profile.zip,
+      profile.region, profile.country, profile.email, profile.phone,
+      profile.gender, profile.birthdate, profile.picture,
+      req.session.uid, req.params.profile
+    ];
+
+    await db.getConnection();
+    const result = await db.query(sql, vars);
+
+    if (!result.affectedRows) throw 'Could not update profile';
     
-    if (!isProfileValid(res, profile)) return;
-    
-    db(cn => {
-        let sql = "UPDATE security SET "
-            + "name = ?, fname = ?, lname = ?, address = ?, zip = ?, region = ?, "
-            + "country = ?, email = ?, phone = ?, gender = ?, birthdate = ?, picture = ? "
-            + "WHERE user_id = ? AND profile_id = ?";
-        let values = [
-            profile.name, profile.fname, profile.lname, profile.address, profile.zip,
-            profile.region, profile.country, profile.email, profile.phone,
-            profile.gender, profile.birthdate, profile.picture,
-            req.session.uid, req.params.profile
-        ];
-        
-        cn.query(sql, values, (err, result) => {
-            cn.release();
-            
-            if (!err)
-                res.json({error: false, message: "Profile successfully updated."});
-            else
-                res.json({error: true, message: "An unknown error occured."});
-        });
-    });
+    db.release();
+    res.json({ error: false, message: 'Profile successfully updated' });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err });
+  }
 
 }
