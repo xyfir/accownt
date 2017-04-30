9ecb01a97af7dd988d0ad8bc70bd87ec39b712a0
MrXyfir <mr@xyfir.com>
2017-04-30 14:07:06 -0700

    Add react-md Toolbar / Drawer. Change code style

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,65 +1,143 @@
-import React from "react";
-import { render } from "react-dom";
+import { render } from 'react-dom';
+import React from 'react';
 
 // Components
-import AccountRecovery from "./containers/AccountRecovery";
-import DynamicStyles from "./misc/DynamicStyles";
-import LoginService from "./containers/LoginService";
-import LinkService from "./containers/LinkService";
-import Dashboard from "./containers/Dashboard";
-import Register from "./containers/Register";
-import Login from "./containers/Login";
+import AccountRecovery from './containers/AccountRecovery';
+import DynamicStyles from './misc/DynamicStyles';
+import LoginService from './containers/LoginService';
+import LinkService from './containers/LinkService';
+import Dashboard from './containers/Dashboard';
+import Register from './containers/Register';
+import Login from './containers/Login';
+
+// react-md
+import Subheader from 'react-md/lib/Subheaders';
+import ListItem from 'react-md/lib/Lists/ListItem';
+import Toolbar from 'react-md/lib/Toolbars';
+import Divider from 'react-md/lib/Dividers';
+import Drawer from 'react-md/lib/Drawers';
+import Button from 'react-md/lib/Buttons/Button';
 
 class App extends React.Component {
-	
-    constructor(props) {
-        super(props);
+  
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            hash: location.hash.split('/')
-        };
+    this.state = {
+      hash: location.hash.split('?')[0].split('/'), drawer: true
+    };
 
-        window.onhashchange = () => {
-            this.setState({ hash: location.hash.split('/') });
-        };
+    window.onhashchange = () =>
+      this.setState({ hash: location.hash.split('?')[0].split('/') });
+  }
+  
+  render() {
+    let view;
+
+    switch (this.state.hash[1]) {
+      case 'recover':
+        view = <AccountRecovery hash={this.state.hash} />; break;
+      
+      case 'login':
+        if (!this.state.hash[2])
+          view = <Login hash={this.state.hash} />;
+        else
+          view = <LoginService hash={this.state.hash} />;
+        break;
+
+      case 'register':
+        if (!this.state.hash[2])
+          view = <Register hash={this.state.hash} />;
+        else
+          view = <LinkService hash={this.state.hash} />;
+        break;
+
+      case 'dashboard':
+        view = <Dashboard hash={this.state.hash} />; break;
+
+      default:
+        location.href = '../';
     }
-	
-	render() {
-        let view;
 
-		switch (this.state.hash[1]) {
-            case "recover":
-                view = <AccountRecovery hash={this.state.hash} />; break;
+    return (
+      <div className='app'>
+        <Toolbar
+          colored
+          title='Xyfir Accounts'
+          nav={
+            <Button
+              icon
+              onClick={() => this.setState({ drawer: true })}
+            >menu</Button>
+          }
+        />
+
+        <Drawer
+          onVisibilityToggle={
+            v => this.setState({ drawer: v })
+          }
+          autoclose={true}
+          navItems={[
+            <Subheader primary primaryText='User Dashboard' />,
+            <a href='#/dashboard/user/account'>
+              <ListItem primaryText='Account' />
+            </a>,
+            <a href='#/dashboard/user/security'>
+              <ListItem primaryText='Security' />
+            </a>,
+            <a href='#/dashboard/user/profiles'>
+              <ListItem primaryText='Profiles' />
+            </a>,
+            <a href='#/dashboard/user/services'>
+              <ListItem primaryText='Services' />
+            </a>,
+            <a href='#/dashboard/user/tokens'>
+              <ListItem primaryText='Tokens' />
+            </a>,
+            <a href='#/dashboard/user/ads'>
+              <ListItem primaryText='Ads' />
+            </a>,
             
-            case "login":
-                if (this.state.hash[2] === undefined)
-                    view = <Login hash={this.state.hash} />;
-                else
-                    view = <LoginService hash={this.state.hash} />;
-                break;
+            <Divider />,
 
-            case "register":
-                if (this.state.hash[2] === undefined)
-                    view = <Register hash={this.state.hash} />;
-                else
-                    view = <LinkService hash={this.state.hash} />;
-                break;
+            <Subheader primary primaryText='Developer Dashboard' />,
+            <a href='#/dashboard/developer/list'>
+              <ListItem primaryText='View Services' />
+            </a>,
+            <a href='#/dashboard/developer/create'>
+              <ListItem primaryText='Create Service' />
+            </a>,
 
-            case "dashboard":
-                view = <Dashboard hash={this.state.hash} />; break;
+            <Divider />,
 
-            default:
-                location.href = "../";
-        }
+            <Subheader primary primaryText='Affiliate Dashboard' />,
+            <a href='#/dashboard/affiliate/list'>
+              <ListItem primaryText='View Campaigns' />
+            </a>,
+            <a href='#/dashboard/affiliate/create'>
+              <ListItem primaryText='Create Campaign' />
+            </a>
+          ]}
+          visible={this.state.drawer}
+          header={
+            <Toolbar
+              colored
+              nav={
+                <Button
+                  icon
+                  onClick={() => this.setState({ drawer: false })}
+                >arrow_back</Button>
+              }
+            />
+          }
+          type={Drawer.DrawerTypes.TEMPORARY}
+        />
 
-        return (
-            <div className="app">
-                {view}
-                <DynamicStyles />
-            </div>
-        )
-	}
-	
+        {view}
+      </div>
+    )
+  }
+  
 }
 
-render(<App />, document.querySelector("#content"));
+render(<App />, document.querySelector('#content'));
