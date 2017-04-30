d26032deea440c833a9834db5a35526f5b248238
MrXyfir <mr@xyfir.com>
2017-04-30 16:15:27 -0700

    Remove /app route. Change code style

--- controllers/login/logout.js
+++ controllers/login/logout.js
@@ -1,15 +1,12 @@
 /*
-    GET api/login/logout
-    DESCRIPTION
-        Destroy user's session
+  GET api/login/logout
+  DESCRIPTION
+    Destroy user's session
 */
 module.exports = function(req, res) {
 
-    req.session.destroy(err => {
-        if (err)
-            res.redirect("/app/#/dashboard");
-        else
-            res.redirect("/app/#/login");
-    });
+  req.session.destroy(err =>
+    res.redirect(err ? '/#/dashboard' : '/#/login')
+  );
 
 }
