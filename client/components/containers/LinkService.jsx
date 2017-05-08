d3cb7a61e8749da21e4633bf1b9641adf477b419
MrXyfir <mr@xyfir.com>
2017-05-08 14:02:43 -0700

    Improvements and implement react-md components
    - Use react-md components
    - Use country-region-data for country / region selectors
    - Hide disabled elements in custom tab
    - Clean up code

--- client/components/containers/LinkService.jsx
+++ client/components/containers/LinkService.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
 import request from 'superagent';
+import React from 'react';
+import crd from 'country-region-data';
 
-// Components
-import Button from 'components/forms/Button';
+// react-md
+import TabsContainer from 'react-md/lib/Tabs/TabsContainer';
+import SelectField from 'react-md/lib/SelectFields';
+import DatePicker from 'react-md/lib/Pickers/DatePickerContainer';
+import Subheader from 'react-md/lib/Subheaders';
+import TextField from 'react-md/lib/TextFields';
+import Checkbox from 'react-md/lib/SelectionControls/Checkbox';
+import ListItem from 'react-md/lib/Lists/ListItem';
+import Button from 'react-md/lib/Buttons/Button';
+import Paper from 'react-md/lib/Papers';
+import List from 'react-md/lib/Lists/List';
+import Tabs from 'react-md/lib/Tabs/Tabs';
+import Tab from 'react-md/lib/Tabs/Tab';
+
+const Conditional = ({show, children}) =>
+  <div style={{ display: show ? 'initial' : 'none' }}>{children}</div>;
 
 export default class LinkService extends React.Component {
   
@@ -10,7 +25,7 @@ export default class LinkService extends React.Component {
     super(props);
 
     this.state = {
-      linked: false, tab: 'profile', id: this.props.hash[2]
+      linked: false, tab: 0, id: this.props.hash[2], country: ''
     };
 
     this._createSession = this._createSession.bind(this);
@@ -26,59 +41,52 @@ export default class LinkService extends React.Component {
             location.hash = "#/login";
         }
         else {
-          if (!res.body.profiles.length)
-            res.body.tab = 'custom';
+          if (!res.body.profiles.length) res.body.tab = 1;
           
           this.setState(res.body);
         }
       });
   }
-
-  onChangeTab(tab) {
-    this.setState({ tab });
-  }
   
+  /**
+   * Link the user's account to the service.
+   * @param {Event} [e]
+   */
   onLink(e) {
     e && e.preventDefault();
 
-    let data = {};
-
-    if (this.state.tab == 'profile') {
-      data = {
-        profile: this.refs.profile.value,
-        required: this.refs.profile_allow_required.checked,
-        optional: this.refs.profile_allow_optional.checked
-      };
-    }
-    else {
-      data = {
-        email: this.refs.email.value,
-        fname: this.refs.fname.value,
-        lname: this.refs.lname.value,
-        gender: this.refs.gender.value,
-        phone: this.refs.phone.value,
-        birthdate: this.refs.birthdate.value,
-        address: this.refs.address.value,
-        zip: this.refs.zip.value,
-        region: this.refs.region.value,
-        country: this.refs.country.value
-      };
-    }
+    const data = this.state.tab == 0 ? {
+      profile: this.refs.profile.state.value,
+      required: window['checkbox-required'].checked,
+      optional: window['checkbox-optional'].checked
+    } : {
+      zip: this.refs.zip.getField().value,
+      email: this.refs.email.getField().value,
+      fname: this.refs.fname.getField().value,
+      lname: this.refs.lname.getField().value,
+      phone: this.refs.phone.getField().value,
+      region: this.refs.region.state.value,
+      gender: this.refs.gender.state.value,
+      country: this.state.country,
+      address: this.refs.address.getField().value,
+      birthdate: this.refs.birthdate.state.value
+    };
   
     request
       .post(`../api/service/${this.state.id}/link`)
       .send(data)
       .end((err, res) => {
-        if (err || res.body.error) {
+        if (err || res.body.error)
           swal('Error', res.body.message, 'error');
-        }
-        else {
-          this.setState({ linked: true });
-          this._createSession();
-        }
+        else
+          this.setState({ linked: true }, () => this._createSession());
       });
   }
   
+  /**
+   * Creates a session linked to the user and service. Redirect the user to 
+   * the service's login page with auth and xid passed.
+   */
   _createSession() {
     request
       .post(`../api/service/${this.state.id}/session`)
@@ -90,216 +98,243 @@ export default class LinkService extends React.Component {
       })
   }
 
+  /**
+   * Check if a data field should be disabled because the service does not 
+   * request it.
+   * @param {string} key
+   * @returns {boolean}
+   */
   _isDisabled(key) {
-    return !this.state.service.requested.required[key]
-      && !this.state.service.requested.optional[key];
+    return !this.state.service.requested.required[key] &&
+      !this.state.service.requested.optional[key];
   }
   
   render() {
-    if (this.state.linked || !this.state.service) {
-      return <div />;
-    }
-    else {
-      const s = this.state.service;
-      
-      return (
-        <div className='link-service service-form-view'>
-          <h2 className='service-name'>{s.name}</h2>
-          <p className='service-description'>{s.description}</p>
+    if (this.state.linked || !this.state.service) return <div />;
+    
+    const s = this.state.service;
+    
+    return (
+      <div className='link-service'>
+        <h2 className='service-name'>{s.name}</h2>
+        <p className='service-description'>{s.description}</p>
 
-          <section className='service-info'>
-            <div className='required'>
-              <span className='title'>Required Information</span>
-              {Object.keys(s.requested.required).map(key => {
-                return (
-                  <dl>
-                    <dt>{key}</dt>
-                    <dd>{s.requested.required[key]}</dd>
-                  </dl>
-                );
-              })}
-            </div>
-            
-            <div className='optional'>
-              <span className='title'>Optional Information</span>
-              {Object.keys(s.requested.optional).length ? (
-                Object.keys(s.requested.optional).map(key => {
-                  return (
-                    <dl>
-                      <dt>{key}</dt>
-                      <dd>{s.requested.optional[key]}</dd>
-                    </dl>
-                  );
-                })
-              ) : (
-                <dl>None</dl>
-              )}
-            </div>
-          </section>
+        <List className='requested-info section md-paper md-paper--2'>
+          <Subheader primary primaryText='Required Information' />
 
-          <section className='link-service'>
-            <nav className='nav-bar'>
-              <a onClick={() => this.onChangeTab('profile')}>
-                Load Data From Profile
-              </a>
-              <a onClick={() => this.onChangeTab('custom')}>
-                Set Custom Data
-              </a>
-            </nav>
+          {Object.keys(s.requested.required).map(key =>
+            <ListItem
+              key={key}
+              primaryText={key}
+              secondaryText={s.requested.required[key]}
+            />
+          )}
 
-            {this.state.tab == 'profile' ? (
-              <form
-                className='profile'
-                onSubmit={(e) => this.onLink(e)}
-              >
-                <p>
-                  Choose a profile and {s.name} will automatically access information you allow from the profile.
-                  
-                  {!this.state.profiles.length ? (
-                    <strong>
-                      <br />
-                      You don't have any profiles to link!
-                    </strong>
-                  ) : (
-                    <span />
-                  )}
+          <Subheader primaryText='Optional Information' />
 
-                  <br />
+          {Object.keys(s.requested.optional).length ? (
+            Object.keys(s.requested.optional).map(key =>
+              <ListItem
+                key={key}
+                primaryText={key}
+                secondaryText={s.requested.optional[key]}
+              />
+            )
+          ) : (
+            <ListItem
+              key='none'
+              primaryText='None'
+              secondaryText='This service does not request any optional info'
+            />
+          )}
+        </List>
 
-                  <a href={
-                    '#/dashboard/user/profiles/create?rdr='
-                    + encodeURIComponent(location.hash)
-                  }>Create New Profile</a>
-                </p>
+        <Paper zDepth={2} className='link-service section'>
+        <TabsContainer
+          colored
+          onTabChange={i => this.setState({ tab: i })}
+          activeTabIndex={this.state.tab}
+        >
+          <Tabs tabId='tab'>
+            <Tab label='Profile'>
+            <form className='profile' onSubmit={e => this.onLink(e)}>
+              <p>
+                Choose a profile and {s.name} will automatically access information you allow from the profile.
                 
-                <select
-                  ref='profile'
-                  className='profile-selector'
-                  defaultValue='0'
-                >
-                  <option value='0'>
-                    Select a profile
-                  </option>
-                  
-                  {this.state.profiles.map(profile => {
-                    return (
-                      <option value={profile.profile_id}>{
-                        profile.name
-                      }</option>
-                    );
-                  })}
-                </select>
-                
-                <div>
-                  <input
-                    type='checkbox'
-                    ref='profile_allow_required'
-                    defaultChecked={true}
-                  />Allow Access to Required Data
-                  <input
-                    type='checkbox'
-                    ref='profile_allow_optional'
-                  />Allow Access to Optional Data
-                </div>
+                {!this.state.profiles.length ? (
+                  <strong>
+                    <br />
+                    You don't have any profiles to link!
+                  </strong>
+                ) : null}
+              </p>
 
-                <Button>Link Service</Button>
-              </form>
-            ) : this.state.tab == 'custom' ? (
-              <form
-                className='custom'
-                onSubmit={(e) => this.onLink(e)}
-              >
-                <p>
-                  Set data that only this service will be able to access.
-                </p>
+              <a href={
+                '#/dashboard/user/profiles/create?rdr='
+                + encodeURIComponent(location.hash)
+              }>Create New Profile</a>
+
+              <br />
               
-                <label>Email</label>
-                <input
-                  type='email'
-                  ref='email'
-                  disabled={this._isDisabled('email')}
-                /> 
-                
-                <br />
-                
-                <label>First Name</label>
-                <input
-                  type='text'
-                  ref='fname'
-                  disabled={this._isDisabled('fname')}
+              <SelectField
+                id='select-profile'
+                ref='profile'
+                label='Profile'
+                menuItems={this.state.profiles}
+                itemLabel='name'
+                itemValue='profile_id'
+                className='md-cell'
+              />
+              
+              <div>
+                <Checkbox
+                  inline
+                  id='checkbox-required'
+                  ref='required'
+                  name='checkbox-required'
+                  label='Allow Access to Required Data'
+                  defaultChecked={true}
                 />
-                
-                <label>Last Name</label>
-                <input
-                  type='text'
-                  ref='lname'
-                  disabled={this._isDisabled('lname')}
+                <Checkbox
+                  inline
+                  id='checkbox-optional'
+                  ref='optional'
+                  name='checkbox-optional'
+                  label='Allow Access to Optional Data'
                 />
-                
-                <label>Gender</label>
-                <select
-                  ref='gender'
-                  disabled={this._isDisabled('gender')}
-                >
-                  <option value='0'>-</option>
-                  <option value='1'>Male</option>
-                  <option value='2'>Female</option>
-                  <option value='3'>Other</option>
-                </select>
-                
-                <label>Phone #</label>
-                <input
-                  type='tel'
-                  ref='phone'
-                  disabled={this._isDisabled('phone')}
-                />
-                
-                <label>Birthdate</label>
-                <input
-                  type='date'
-                  ref='birthdate'
-                  disabled={this._isDisabled('birthdate')}
-                />
-                
-                <br />
-                
-                <label>Address</label>
-                <input
-                  type='text'
-                  ref='address'
-                  disabled={this._isDisabled('address')}
-                />
-                
-                <label>Zip Code</label>
-                <input
-                  type='number'
-                  ref='zip'
-                  disabled={this._isDisabled('zip')}
-                />
-                
-                <label>State/Province/Region Code</label>
-                <input
-                  type='text'
-                  ref='region'
-                  disabled={this._isDisabled('region')}
-                />
-                
-                <label>Country Code</label>
-                <input
-                  type='text'
-                  ref='country'
-                  disabled={this._isDisabled('country')}
-                />
+              </div>
 
-                <Button>Link Service</Button>
-              </form>
-            ) : (
-              <div />
-            )}
-          </section>
-        </div>
-      );
-    }
+              <Button
+                raised primary
+                onClick={e => this.onLink()}
+                label='Link Service'
+              />
+            </form>
+            </Tab>
+
+            <Tab label='Custom Data'>
+            <form className='custom' onSubmit={e => this.onLink(e)}>
+              <p>
+                Set data that only this service will be able to access.
+              </p>
+            
+              <Conditional show={!this._isDisabled('email')}>
+                <TextField
+                  id='email--email'
+                  ref='email'
+                  type='email'
+                  label='Email'
+                />
+              </Conditional>
+              
+              <Conditional show={!this._isDisabled('fname')}>
+                <TextField
+                  id='text--fname'
+                  ref='fname'
+                  type='text'
+                  label='First Name'
+                />
+              </Conditional>
+              
+              <Conditional show={!this._isDisabled('lname')}>
+                <TextField
+                  id='text--lname'
+                  ref='lname'
+                  type='text'
+                  label='Last Name'
+                />
+              </Conditional>
+              
+              <Conditional show={!this._isDisabled('gender')}>
+                <SelectField
+                  fullWidth
+                  id='select-gender'
+                  ref='gender'
+                  label='Gender'
+                  menuItems={[
+                    { label: '-', value: 0 },
+                    { label: 'Male', value: 1 },
+                    { label: 'Female', value: 2 },
+                    { label: 'Other', value: 3 }
+                  ]}
+                />
+              </Conditional>
+              
+              <Conditional show={!this._isDisabled('phone')}>
+                <TextField
+                  id='tel--phone'
+                  ref='phone'
+                  type='tel'
+                  label='Phone #'
+                />
+              </Conditional>
+              
+              <Conditional show={!this._isDisabled('birthdate')}>
+                <DatePicker
+                  id='date--birthdate'
+                  ref='birthdate'
+                  label='Birthdate'
+                />
+              </Conditional>
+
+              <Conditional show={!this._isDisabled('country')}>
+                <SelectField
+                  fullWidth
+                  id='select-country'
+                  label='Country'
+                  value={this.state.country}
+                  onChange={v => this.setState({ country: v })}
+                  menuItems={crd}
+                  itemLabel='countryName'
+                  itemValue='countryShortCode'
+                />
+              </Conditional>
+              
+              <Conditional show={!this._isDisabled('region')}>
+                <SelectField
+                  fullWidth
+                  id='select-region'
+                  ref='region'
+                  label='State/Province/Region'
+                  menuItems={
+                    (crd.find(c => c.countryShortCode == this.state.country) ||
+                    { regions: [] }).regions
+                  }
+                  itemLabel='name'
+                  itemValue='shortCode'
+                />
+              </Conditional>
+              
+              <Conditional show={!this._isDisabled('address')}>
+                <TextField
+                  id='text--address'
+                  ref='address'
+                  type='text'
+                  label='Address'
+                />
+              </Conditional>
+              
+              <Conditional show={!this._isDisabled('zip')}>
+                <TextField
+                  id='text--zip'
+                  ref='zip'
+                  type='text'
+                  label='Zip Code'
+                />
+              </Conditional>
+
+              <Button
+                raised primary
+                onClick={e => this.onLink()}
+                label='Link Service'
+              />
+            </form>
+            </Tab>
+          </Tabs>
+        </TabsContainer>
+        </Paper>
+      </div>
+    );
   }
   
 }
