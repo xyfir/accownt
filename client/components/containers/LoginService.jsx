73105838e5cdf174422de6816117ab066744a7f2
MrXyfir <mr@xyfir.com>
2017-05-10 09:16:15 -0700

    Improvements, changes
    - Change code style
    - Use superagent
    - Update _createSession() to reflect API changes
    - Update output

--- client/components/containers/LoginService.jsx
+++ client/components/containers/LoginService.jsx
@@ -1,62 +1,56 @@
-import React from "react";
-
-// Components
-import Alert from "../misc/Alert";
-
-// Modules
-import request from "../../lib/request";
+import request from 'superagent';
+import React from 'react';
 
 export default class LoginService extends React.Component {
-	
-	constructor(props) {
-		super(props);
+  
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			error: false, message: "",
-			service: this.props.hash[2]
-		};
+    this.state = {
+      message: '', service: this.props.hash[2]
+    };
 
-		this._createSession = this._createSession.bind(this);
-	}
-	
-	componentWillMount() {
-		request({
-			url: "../api/service/" + this.state.service,
-			success: data => {
-				// User is not logged in
-				// After login user will be redirect back here
-				if (data.error && data.message == "Not logged in")
-					location.hash = "/login";
-				// Create session
-				else if (data.error && data.message.indexOf("already linked") > -1)
-					this._createSession();
-				// User hasn't linked service yet
-				else
-					location.hash = "/register/" + this.state.service;
-			}
-		});
-	}
-	
-	_createSession() {
-		request({
-			url: `../api/service/${this.state.service}/session`,
-			method: "POST",
-			success: data => {
-				this.setState({
-					error: false, message: "Logged in successfully."
-				});
-				
-				// Redirect user to service's login
-				location.href = data.address + "?auth=" + data.auth + "&xid=" + data.xid;
-			}
-		});
-	}
-	
-	render() {
-		if (this.state.message)
-			return <div className='old'><Alert type="success" title="Success!">{this.state.message}</Alert></div>;
-		else
-			return <div />;
-	}
-	
+    this._createSession = this._createSession.bind(this);
+  }
+  
+  /**
+   * Check if user is logged in and linked to the service.
+   */
+  componentWillMount() {
+    request
+      .get('api/service/' + this.state.service)
+      .end((err, res) => {
+        if (err)
+          location.hash = '#/';
+        // User is not logged in
+        // After login user will be redirect back here
+        else if (res.body.error && res.body.message == 'Not logged in')
+          location.hash = '#/login';
+        // Create session
+        else if (
+          res.body.error && res.body.message.indexOf('already linked') > -1
+        )
+          this._createSession();
+        // User hasn't linked service yet
+        else
+          location.hash = '#/register/' + this.state.service;
+      });
+  }
+  
+  /**
+   * Creates a session linked to the user and service. Redirect the user to 
+   * the service's login route.
+   */
+  _createSession() {
+    request
+      .post(`api/service/${this.state.service}/session`)
+      .end((err, res) =>
+        !err && location.replace(res.body.redirect)
+      );
+  }
+  
+  render() {
+    return <h2>{this.state.message}</h2>;
+  }
+  
 }
