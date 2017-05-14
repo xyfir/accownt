e0c68289b61d08691e743fcc25cac75008374fbe
MrXyfir <mr@xyfir.com>
2017-05-14 14:36:28 -0700

    Improvements, changes
    - Move components/containers/Register code to components/register/Account
    - Modify Register container to handle #/register routes
    - Update RegisterAccount code style / use superagent / react-md

--- client/components/containers/Register.jsx
+++ client/components/containers/Register.jsx
@@ -1,141 +1,26 @@
-import React from "react";
+import React from 'react';
 
 // Components
-import Button from "components/forms/Button";
-
-// Modules
-import request from "lib/request";
-
-// Constants
-import { RECAPTCHA_KEY } from "constants/config";
+import Account from 'components/register/Account';
+import Service from 'components/register/Service';
 
 export default class Register extends React.Component {
-	
-	constructor(props) {
-		super(props);
-
-		this.state = { error: false, message: "", created: false };
-
-		// Load reCAPTCHA lib
-        let element = document.createElement("script");
-        element.src = "https://www.google.com/recaptcha/api.js";
-        element.type = "text/javascript";
-        document.body.appendChild(element);
-	}
-
-	onSubmit() {
-		const data = {
-			email: this.refs.email.value,
-			password: this.refs.password.value,
-			passwordr: this.refs.passwordr.value,
-			recaptcha: grecaptcha.getResponse()
-		};
-		
-		if (data.password != data.passwordr) {
-			this.setState({ error: true, message: "Passwords do not match." });
-		}
-		else {
-			// Attempt to register user
-			request({
-				url: "../api/register",
-				method: "POST", data, success: (result) => {
-					if (result.error)
-						this.setState(result);
-					else
-						this.setState({ created: true });
-				}
-			});
-		}
-	}
-
-	onCheckEmail() {
-		clearTimeout(this.checkEmailTimeout);
-
-		if (this.refs.email.value.length) {
-			// Check if email is available
-			this.checkEmailTimeout = setTimeout(() => {
-				request({
-					url: "../api/register/email/" + this.refs.email.value,
-					dataType: "text", success: (result) => {
-						if (result == 1) {
-							this.setState({
-								error: true,
-								message: "Email is already linked to an account."
-							});
-						}
-						else {
-							this.setState({ error: false, message: "" });
-						}
-					}
-				});
-			}, 200);
-		}
-	}
-	
-	render() {
-		if (this.state.created) {
-			return (
-				<div className="register account-created old">
-					<section className="info">
-						<span>
-							Account created successfully. A verification link has been sent to your email.
-							<br />
-							You will not be able to login until you verify your email.
-						</span>
-					</section>
-
-					<section className="controls">
-						<Button type="primary" onClick={
-							() => location.hash = "#/login"
-						}>Continue to Login</Button>
-					</section>
-				</div>
-			)
-		}
-
-		return (
-			<div className="register form-step-body old">
-				<span className={this.state.error ? "error" : ""}>{
-					this.state.message
-				}</span>
-
-				<section className="fields">
-					<label>Email</label>
-					<span className="input-description">
-						Your email will be used to login to your account, receive notifications, and for account recovery.
-					</span>
-					<input
-						ref="email"
-						type="email"
-						onChange={() => this.onCheckEmail()}
-						placeholder="user@email.com"
-					/>
-
-					<label>Password</label>
-					<input
-						ref="password"
-						type="password"
-					/>
-					<input
-						ref="passwordr"
-						type="password"
-					/>
-				</section>
-
-				<section className="recaptcha-wrapper">
-					<div
-						className="g-recaptcha"
-						data-sitekey={RECAPTCHA_KEY}
-					/>
-				</section>
-
-				<section className="controls">
-					<button onClick={() => this.onSubmit()} className="btn-primary">
-						Create account
-					</button>
-				</section>
-			</div>
-		);
-	}
-
+  
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    switch (this.props.hash[2]) {
+      case 'service':
+        return <Service />;
+      default:
+        // Support old service registration links at `#/register/:id`
+        if (this.props.hash[2])
+          return <Service />;
+        else
+          return <Account />;
+    }
+  }
+  
 }
---
+++ client/components/register/Account.jsx
@@ -0,0 +1,144 @@
+import request from 'superagent';
+import React from 'react';
+
+// Constants
+import { RECAPTCHA_KEY } from 'constants/config';
+
+// react-md
+import TextField from 'react-md/lib/TextFields';
+import Button from 'react-md/lib/Buttons/Button';
+import Paper from 'react-md/lib/Papers';
+
+export default class RegisterAccount extends React.Component {
+	
+	constructor(props) {
+		super(props);
+
+		this.state = { created: false, emailTaken: false };
+
+		// Load reCAPTCHA lib
+		const element = document.createElement('script');
+		element.src = 'https://www.google.com/recaptcha/api.js';
+		element.type = 'text/javascript';
+		document.head.appendChild(element);
+	}
+
+	onCreate() {
+		const data = {
+			email: this.refs.email.getField().value,
+			password: this.refs.password.getField().value,
+			passwordr: this.refs.passwordr.getField().value,
+			recaptcha: grecaptcha.getResponse()
+		};
+		
+		if (data.password != data.passwordr) {
+      swal('Error', 'Passwords do not match.', 'error');
+		}
+		else {
+			// Attempt to register user
+      request
+        .post('api/register')
+        .send(data)
+        .end((err, res) => {
+          if (err || res.body.error)
+            swal('Error', res.body.message, 'error');
+					else
+						this.setState({ created: true });
+        });
+		}
+	}
+
+	onCheckEmail() {
+		clearTimeout(this.checkEmailTimeout);
+
+    const email = this.refs.email.getField().value;
+
+		if (email) {
+			// Check if email is available
+			this.checkEmailTimeout = setTimeout(() =>
+        request
+          .get('api/register/email')
+          .query({ email })
+          .end((err, res) =>
+            this.setState({ emailTaken: res.body.exists })
+          )
+			, 200);
+		}
+	}
+	
+	render() {
+		if (this.state.created) {
+			return (
+				<div className='register account-created'>
+          <span>
+            Account created successfully. A verification link has been sent to your email.
+            <br />
+            You will not be able to login until you verify your email.
+            <br />
+          </span>
+
+          <Button
+            raised primary
+            label='Continue to Login'
+            onClick={() => location.hash = '#/login'}
+          />
+				</div>
+			)
+		}
+
+		return (
+			<form className='register'>
+        {this.state.emailTaken ? (
+          <span className='email-taken'>
+            An account with that email already exists. <a href='#/login'>Login?</a>
+          </span>
+        ) : null}
+
+				<Paper zDepth={1} className='section flex'>
+          <TextField
+            id='email'
+            ref='email'
+            type='email'
+            label='Email'
+            onChange={() => this.onCheckEmail()}
+            helpText={
+              'Used to login to your account, receive notifications, and for \
+              account recovery.'
+            }
+            className='md-cell'
+          />
+
+          <TextField
+            id='password--pass'
+            ref='password'
+            type='password'
+            label='Password'
+            className='md-cell'
+          />
+
+					<TextField
+            id='password--conf'
+            ref='passwordr'
+            type='password'
+            label='Confirm Password'
+            className='md-cell'
+          />
+
+          <div className='recaptcha-wrapper'>
+            <div
+              className='g-recaptcha'
+              data-sitekey={RECAPTCHA_KEY}
+            />
+          </div>
+
+          <Button
+            raised primary
+            label='Create Account'
+            onClick={() => this.onCreate()}
+          />
+				</Paper>
+			</form>
+		);
+	}
+
+}
