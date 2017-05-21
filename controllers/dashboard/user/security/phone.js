8f38ded8ed26d13e50883fb5f645c19b4e019a25
MrXyfir <mr@xyfir.com>
2017-05-21 12:17:40 -0700

    Improvements, changes
    - Update code style
    - Use lib/mysql
    - Improve output
    - Do not set phone until verified unless the phone is being removed

--- controllers/dashboard/user/security/phone.js
+++ controllers/dashboard/user/security/phone.js
@@ -1,24 +1,42 @@
-const sendCode = require("lib/sms/send-code");
-const db = require("lib/db");
+const sendCode = require('lib/sms/send-code');
+const mysql = require('lib/mysql');
 
 /*
-    PUT api/dashboard/user/security/phone
-    REQUIRED
-        phone: string
-    RETURN
-        { error: bool, message: string }
+  PUT api/dashboard/user/security/phone
+  OPTIONAL
+    phone: string
+  RETURN
+    { error: bool, message: string }
 */
-module.exports = function(req, res) {
-		
-    db(cn => {
-        cn.query("UPDATE security SET phone = ? WHERE user_id = ?", [req.body.phone, req.session.uid], (err, result) => {
-            cn.release();
-            
-            if (err || !result.affectedRows)
-                res.json({ error: true, message: "An unknown error occured" });
-            else if (req.body.phone != 0)
-                sendCode(req.session.uid, req.body.phone);
-        });
-    });
+module.exports = async function(req, res) {
+
+  const db = new mysql;
+
+  try {
+    // User is adding or changing phone
+    // New phone needs to be verified
+    if (req.body.phone) {
+      await sendCode(req.session.uid, req.body.phone);
+      res.json({ error: false, message: 'Verification code sent' });
+    }
+    // Remove existing phone from account
+    else {
+      await db.getConnection();
+
+      const result = await db.query(
+        'UPDATE security SET phone = ? WHERE user_id = ?',
+        ['', req.session.uid]
+      );
+      db.release();
+
+      if (!result.affectedRows) throw 'Could not remove phone';
+
+      res.json({ error: false, message: 'SMS 2FA disabled' });
+    }
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err });
+  }
 
 }
