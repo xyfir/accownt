65daac936627740c7d993d722994265abe1eb195
MrXyfir <mr@xyfir.com>
2017-05-21 16:10:33 -0700

    Improve output. Add `appOtp` to output object

--- controllers/dashboard/user/security/info.js
+++ controllers/dashboard/user/security/info.js
@@ -3,7 +3,10 @@ const mysql = require('lib/mysql');
 /*
   GET api/dashboard/user/security
   RETURN
-    { error: boolean, phone: string, codes: string, passwordless: number }
+    {
+      error: boolean,
+      phone: string, codes: string, passwordless: number, appOtp: boolean
+    }
 */
 module.exports = async function(req, res) {
 
@@ -12,12 +15,18 @@ module.exports = async function(req, res) {
   try {
     await db.getConnection();
 
-    const rows = await db.query(
-      'SELECT * FROM security WHERE user_id = ?',
-      [req.session.uid]
-    );
+    const sql = `
+      SELECT
+        phone, codes, passwordless, IF(LENGTH(otp_secret) > 0, 1, 0) AS appOtp
+      FROM security WHERE user_id = ?
+    `,
+    vars = [
+      req.session.uid
+    ],
+    rows = await db.query(sql, vars);
     db.release();
 
+    rows[0].appOtp = !!rows[0].appOtp;
     res.json(rows[0]);
   }
   catch (err) {
