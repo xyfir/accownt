ee387a84d133f76605c856cee74a5c83cb5192df
MrXyfir <mr@xyfir.com>
2017-05-22 14:38:35 -0700

    Add `google` to return object

--- controllers/dashboard/user/security/info.js
+++ controllers/dashboard/user/security/info.js
@@ -5,7 +5,8 @@ const mysql = require('lib/mysql');
   RETURN
     {
       error: boolean,
-      phone: string, codes: string, passwordless: number, appOtp: boolean
+      phone: string, codes: string, passwordless: number, appOtp: boolean,
+      google: boolean
     }
 */
 module.exports = async function(req, res) {
@@ -17,16 +18,18 @@ module.exports = async function(req, res) {
 
     const sql = `
       SELECT
-        phone, codes, passwordless, IF(LENGTH(otp_secret) > 0, 1, 0) AS appOtp
+        phone, codes, passwordless, IF(LENGTH(otp_secret) > 0, 1, 0) AS appOtp,
+        (SELECT google FROM users WHERE id = ?) AS google
       FROM security WHERE user_id = ?
     `,
-    vars = [
-      req.session.uid
-    ],
+    vars = new Array(2).fill(req.session.uid),
     rows = await db.query(sql, vars);
     db.release();
 
-    rows[0].appOtp = !!rows[0].appOtp;
+    rows[0].google = !!rows[0].google,
+    rows[0].appOtp = !!rows[0].appOtp,
+    rows[0].error = false;
+
     res.json(rows[0]);
   }
   catch (err) {
