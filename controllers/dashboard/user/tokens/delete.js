8dfd1df7ef8654a03d1c2b80756b08398b204e4b
MrXyfir <mr@xyfir.com>
2017-05-23 12:42:12 -0700

    Improvements, changes
    - Update code style
    - Use lib/mysql
    - Support deleting all tokens

--- controllers/dashboard/user/tokens/delete.js
+++ controllers/dashboard/user/tokens/delete.js
@@ -1,27 +1,52 @@
-const db = require("lib/db");
+const mysql = require('lib/mysql');
 
 /*
-    DELETE api/dashboard/user/tokens
-    REQUIRED
-        service: number, token: string
-    RETURN
-        { error: boolean }
-    DESCRIPTION
-        Delete an access token
+  DELETE api/dashboard/user/tokens
+  REQUIRED
+    service: number, token: string
+    OR
+    all: boolean
+  RETURN
+    { error: boolean }
+  DESCRIPTION
+    Delete a single, or all access tokens
 */
 
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
 
-    let sql = `
+  const db = new mysql;
+
+  try {
+    await db.getConnection();
+
+    let sql, vars;
+
+    if (req.body.all) {
+      sql = `
+        DELETE FROM access_tokens WHERE user_id = ?
+      `,
+      vars = [
+        req.session.uid
+      ];
+    }
+    else {
+      sql = `
         DELETE FROM access_tokens
         WHERE user_id = ? AND service_id = ? AND token = ?
-    `, vars = [
+      `,
+      vars = [
         req.session.uid, req.body.service, req.body.token
-    ];
+      ];
+    }
 
-    db(cn => cn.query(sql, vars, (err, result) => {
-        cn.release();
-        res.json({ error: !!err || !result.affectedRows });
-    }));
+    const result = await db.query(sql, vars);
+    db.release();
+    
+    res.json({ error: false });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true });
+  }
 
 }
