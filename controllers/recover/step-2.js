1cfe7b48f9c03659d69017700d65536c99838b49
MrXyfir <mr@xyfir.com>
2017-05-22 17:12:03 -0700

    Support recovery code to bypass 2FA

--- controllers/recover/step-2.js
+++ controllers/recover/step-2.js
@@ -1,19 +1,35 @@
 const securityValidation = require('lib/security/validate');
 const sendRecoveryEmail = require('lib/email/send-recovery');
+const mysql = require('lib/mysql');
 
 /*
   POST api/recover/verify
   REQUIRED
-    email: string, uid: number, auth: string
+    uid: number, auth: string
   OPTIONAL
-    code: string, smsCode: string, otpCode: string
+    code: string, smsCode: string, otpCode: string, recovery: string
   RETURN
     { error: bool, message: string }
 */
 module.exports = async function(req, res) {
+
+  const db = new mysql;
   
   try {
-    await securityValidation(req.body);
+    // User provided recovery code
+    if (req.body.recovery) {
+      await db.getConnection();
+      const rows = await db.query(
+        'SELECT user_id FROM security WHERE user_id = ? AND recovery = ?',
+        [req.body.uid, req.body.recovery]
+      );
+      db.release();
+
+      if (!rows.length) throw 'Invalid recovery code';
+    }
+    else {
+      await securityValidation(req.body);
+    }
 
     // Send account recovery email link
     sendRecoveryEmail(req.body.uid, req.body.email);
@@ -24,6 +40,7 @@ module.exports = async function(req, res) {
     });
   }
   catch (err) {
+    db.release();
     res.json({ error: true, message: err });
   }
   
