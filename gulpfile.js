1ad683075eb9ad558b0741429fbedb3f4463bd55
MrXyfir <mr@xyfir.com>
2017-10-02 14:29:30 -0700

    Updates / improvements

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,14 @@ const config = require('./config');
 const prod = config.environment.type == 'prod';
 process.env.NODE_ENV = prod ? 'production' : config.environment.type;
 
-gulp.task('css', () =>
-  gulp.src('./client/styles/style.css')
-    .pipe(
-      sass({ outputStyle: 'compressed' }).on('error', sass.logError)
-    )
-    .pipe(gulp.dest('./static/css'))
+gulp.task('css', () => gulp
+  .src('./client/styles/style.css')
+  .pipe(sass({ outputStyle: 'compressed' })
+  .on('error', sass.logError))
+  .pipe(gulp.dest('./static/css'))
 );
 
-gulp.task('client', () => {
+gulp.task('js', () => {
   const browserify = require('browserify');
   const streamify = require('gulp-streamify');
   const babelify = require('babelify');
@@ -29,24 +28,24 @@ gulp.task('client', () => {
       debug: true, extensions, paths: ['./client']
     }
   );
-  b.transform(babelify.configure({
-    extensions, presets: ['es2015', 'react']
-  }));
+  b.transform(
+    babelify.configure({ extensions, presets: ['env', 'react'] })
+  );
   
   return b.bundle()
     .pipe(source('App.js'))
-    .pipe(
-      prod ? streamify(uglify({
-        mangle: false,
-        compress: { unused: false }
-      }))
-      .on('error', gutil.log) : gutil.noop()
+    .pipe(prod
+      ? streamify(
+          uglify({ mangle: false, compress: { unused: false } })
+        )
+        .on('error', gutil.log)
+      : gutil.noop()
     )
     .pipe(gulp.dest('./static/js/'));
 });
 
-gulp.task('fontello', () =>
-  gulp.src('fontello.json')
-    .pipe(require('gulp-fontello')())
-    .pipe(gulp.dest('./static/fontello'))
+gulp.task('fontello', () => gulp
+  .src('fontello.json')
+  .pipe(require('gulp-fontello')())
+  .pipe(gulp.dest('./static/fontello'))
 );
