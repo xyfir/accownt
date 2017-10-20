8745372b191dc34a4188ca35855ae8a47b421b4c
MrXyfir <mr@xyfir.com>
2017-10-20 14:32:33 -0700

    Use react-md

--- client/components/dashboard/affiliate/List.jsx
+++ client/components/dashboard/affiliate/List.jsx
@@ -2,12 +2,19 @@ import request from 'superagent';
 import React from 'react';
 import swal from 'sweetalert';
 
+// react-md
+import ListItem from 'react-md/lib/Lists/ListItem';
+import Button from 'react-md/lib/Buttons/Button';
+import Dialog from 'react-md/lib/Dialogs';
+import Paper from 'react-md/lib/Papers';
+import List from 'react-md/lib/Lists/List';
+
 export default class ListAffiliateCampaigns extends React.Component {
 
   constructor(props) {
     super(props);
 
-    this.state = { campaigns: [] };
+    this.state = { campaigns: [], selected: -1 };
   }
 
   componentWillMount() {
@@ -16,75 +23,75 @@ export default class ListAffiliateCampaigns extends React.Component {
       .end((err, res) => !err && this.setState(res.body));
   }
 
+  /** @param {string} code */
   onDelete(code) {
     swal({
       title: 'Are you sure?',
       text: 'This action cannot be undone. All unpaid earnings will be lost.',
       icon: 'warning',
       button: 'Delete'
-    }, () =>
-      request
-        .delete('/api/dashboard/affiliate/' + code)
-        .end((err, res) => {
-          if (err || res.body.error)
-            setTimeout(() => swal('Error', 'Could not delete', 'error'), 1000);
-          else
-            location.reload();
-        })
-    );
+    })
+    .then(() => request.delete('/api/dashboard/affiliate/' + code))
+    .then(res => {
+      if (res.body.error) {
+        swal.close();
+        swal('Error', 'Could not delete', 'error');
+      }
+      else {
+        location.reload();
+      }
+    });
   }
 
   render() {
+    const selected =
+      this.state.campaigns[this.state.selected] ||
+      { promo: { link: '' } };
+
     return (
       <div className='campaigns-list'>
-        <section className='notes'>
-          <p>
-            Statistics are only for current payment cycle.
-            <br />
-            Once the next payments are sent out these values will be reset.
-          </p>
-        </section>
+        <p>
+          Statistics are only for the current payment cycle.
+          <br />
+          Once the next payments are sent out these values will be reset.
+        </p>
 
-        <section className='campaigns'>{
-          this.state.campaigns.map(c => {
-            const link = c.promo.link.replace('%CODE%', c.code);
+        <List className='md-paper md-paper--1 section'>{
+          this.state.campaigns.map((c, i) =>
+            <ListItem
+              key={c.code}
+              onClick={() => this.setState({ selected: i })}
+              primaryText={`[${c.code}] ${c.promo.name}`}
+              secondaryText={c.promo.description}
+            />
+          )
+        }</List>
 
-            return (
-              <div className='campaign' key={c.code}>
-                <span className='code'>{c.code}</span>
-                <span className='promo-name'>{
-                  c.promo.name
-                }</span>
+        <Dialog
+          id='selected-affiliate-campaign'
+          title={selected.promo.name}
+          onHide={() => this.setState({ selected: -1 })}
+          visible={this.state.selected > -1}
+          aria-label='Selected Campaign'
+        >
+          <a
+            href={selected.promo.link.replace('%CODE%', selected.code)}
+            target='_blank'
+            className='promo-link'
+          >Link</a>
 
-                <a
-                  href={link}
-                  target='_blank'
-                  className='promo-link'
-                >{link}</a>
-                
-                <span className='promo-description'>{
-                  c.promo.description
-                }</span>
+          <ul>
+            <li>{selected.signups} Signups</li>
+            <li>{selected.purchases} Purchases</li>
+            <li>${selected.earnings} Earnings</li>
+          </ul>
 
-                <span className='signups'>
-                  {c.signups} Signups
-                </span>
-                <span className='purchases'>
-                  {c.purchases} Purchases
-                </span>
-                <span className='earnings'>
-                  ${c.earnings} Earnings
-                </span>
-
-                <a
-                  title='Delete Campaign'
-                  className='icon-delete'
-                  onClick={() => this.onDelete(c.code)}
-                >Delete Campaign</a>
-              </div>
-            )
-          })
-        }</section>
+          <Button
+            raised secondary
+            onClick={() => this.onDelete(selected.code)}
+            iconChildren='delete'
+          >Delete</Button>
+        </Dialog>
       </div>
     );
   }
