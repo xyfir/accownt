f645131f3891abe697f07fc21e2001194f88595f
MrXyfir <mr@xyfir.com>
2017-10-27 20:10:37 -0700

    Fix css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 
-gulp.task('css', () => gulp
-  .src('./client/styles/style.css')
-  .pipe(require('gulp-sass')({ outputStyle: 'compressed' })
-  .on('error', sass.logError))
-  .pipe(gulp.dest('./static/css'))
-);
+gulp.task('css', () => {
+  const sass = require('gulp-sass');
+  
+  return gulp
+    .src('./client/styles/style.css')
+    .pipe(sass({ outputStyle: 'compressed' })
+    .on('error', sass.logError))
+    .pipe(gulp.dest('./static/css'));
+});
 
 gulp.task('fontello', () => gulp
   .src('fontello.json')
