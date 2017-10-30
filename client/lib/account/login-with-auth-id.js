c41b3bcef5b237854c1b2c76ca9f447503ea9feb
MrXyfir <mr@xyfir.com>
2017-10-30 12:32:04 -0700

    Improvements

--- client/lib/account/login-with-auth-id.js
+++ client/lib/account/login-with-auth-id.js
@@ -8,22 +8,24 @@ import request from 'superagent';
  */
 export default function(userId, authId) {
 
- let tries = 0;
+  // 10 minutes in the future
+  const stopAfter = Date.now() + ((60 * 10) * 1000);
 
- const interval = setInterval(() => request
-   .post('/api/login/auth-id')
-   .send({
-     userId, authId
-   })
-   .end((err, res) => {
-     if (!err && !res.body.error) {
-       clearInterval(interval);
-       location.href = res.body.redirect;
-     }
-     else if (++tries >= 85) {
-       clearInterval(interval);
-     }
-   })
- , 7000);
+  const interval = setInterval(() =>
+    document.hasFocus() && request
+      .post('/api/login/auth-id')
+      .send({
+        userId, authId
+      })
+      .end((err, res) => {
+        if (!err && !res.body.error) {
+          clearInterval(interval);
+          location.href = res.body.redirect;
+        }
+        else if (Date.now() >= stopAfter) {
+          clearInterval(interval);
+        }
+      })
+  , 5000);
 
 }
