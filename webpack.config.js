fdf7e5b1286f68fc2f09d70639593e8f1aaf10b0
MrXyfir <mr@xyfir.com>
2017-10-30 13:20:45 -0700

    Add production plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const CompressionPlugin = require('compression-webpack-plugin');
 const webpack = require('webpack');
 const config = require('./config');
 const path = require('path');
@@ -7,9 +8,17 @@ const isProd = config.environment.type == 'prod';
 
 if (isProd) {
   plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin({
       mangle: false,
       compress: { unused: false }
+    }),
+    new CompressionPlugin({
+      asset: '[path].gz'
     })
   );
 }
