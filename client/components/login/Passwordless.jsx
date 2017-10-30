ae8534f54d34b3930c103b63ff22d78bb027d2fc
MrXyfir <mr@xyfir.com>
2017-10-29 17:23:13 -0700

    Improvements, changes
    - Integrate `loginWithAuthId()`.
    - Code improvements
    - Remove `onLogin()` and 'Authorization Code' input

--- client/components/login/Passwordless.jsx
+++ client/components/login/Passwordless.jsx
@@ -7,6 +7,7 @@ import TextField from 'react-md/lib/TextFields';
 import Button from 'react-md/lib/Buttons/Button';
 
 // Modules
+import loginWithAuthId from 'lib/account/login-with-auth-id';
 import query from 'lib/url/parse-hash-query';
 
 // Constants
@@ -24,78 +25,52 @@ export default class PasswordlessLogin extends React.Component {
    * Sends the passwordless login link to the provided email.
    */
   onSend() {
-    const email = this.state.email;
-
     request
-      .get('api/login/passwordless/' + email)
+      .get('api/login/passwordless')
+      .query({ email: this.state.email })
       .end((err, res) => {
-        if (err && res.body.error)
-          swal('Error', res.body.message, 'error');
-        else
-          this.setState({ sent: true });
+        if (err || res.body.error)
+          return swal('Error', res.body.message, 'error');
+
+        this.setState({ sent: true });
+        loginWithAuthId(res.body.userId, res.body.authId);
       });
   }
 
-  /**
-   * Attempts to login using the passwordless login authentication token.
-   */
-  onLogin() {
-    const c = this.refs.code.value.split('_');
-
-    location.href = `${XACC}api/login/passwordless/${c[0]}/${c[1]}`;
-  }
-
   render() {
-    if (!this.state.sent) {
-      return (
-        <div className='login-passwordless step-1'>
-          <h2>Passwordless Login</h2>
-          <p>
-            Enter the email you use to login with. Emails that are only linked to a profile and not your actual account will not work.
-          </p>
-
-          <form className='md-paper md-paper--1 section flex'>
-            <TextField
-              id='email--email'
-              type='email'
-              label='Account Email'
-              value={this.state.email}
-              onChange={v => this.setState({ email: v })}
-              className='md-cell'
-            />
-
-            <Button
-              raised primary
-              onClick={() => this.onSend()}
-            >Next</Button>
-          </form>
-        </div>
-      );
-    }
-
-    return (
-      <div className='login-passwordless step-2'>
+    if (!this.state.sent) return (
+      <div className='login-passwordless step-1'>
         <h2>Passwordless Login</h2>
         <p>
-          A message was sent to your email that contains a passwordless login link and an authorization code.
-          <br />
-          Click the link in the message to bypass this step or copy and paste the authorization code below.
+          Enter the email you use to login with. Emails that are only linked to a profile and not your actual account will not work.
         </p>
-        
+
         <form className='md-paper md-paper--1 section flex'>
           <TextField
-            id='text--code'
-            ref='code'
-            type='text'
-            label='Authorization Code'
+            id='email--email'
+            type='email'
+            label='Account Email'
+            value={this.state.email}
+            onChange={v => this.setState({ email: v })}
             className='md-cell'
           />
 
           <Button
             raised primary
-            onClick={() => this.onLogin()}
-          >Login</Button>
+            onClick={() => this.onSend()}
+          >Next</Button>
         </form>
+      </div>
+    );
+    else return (
+      <div className='login-passwordless step-2'>
+        <h2>Passwordless Login</h2>
+        <p>
+          A message was sent to your email that contains a passwordless login link.
+        </p>
+        <p>
+          Clicking the link in the message will log you in here, and also wherever you clicked the link.
+        </p>
       </div>
     );
   }
