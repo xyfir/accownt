ee3d9c88a3ff409df40be9d4a805ffcfdaf53e93
MrXyfir <mr@xyfir.com>
2017-11-09 14:50:07 -0800

    Add Admyn admin panel

---
+++ client/components/Admin.jsx
@@ -0,0 +1,8 @@
+import AdminPanel from 'admyn/client';
+import { render } from 'react-dom';
+import React from 'react';
+
+render(
+  <AdminPanel title='xyAccounts Admin' api='/admyn-DU9oF5p690ojUKdR3NYS/' />,
+  document.getElementById('content')
+);
