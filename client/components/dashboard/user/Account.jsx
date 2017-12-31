924619e323f831f29c0cf728cef7a4b8c54fc981
MrXyfir <mr@xyfir.com>
2017-12-30 16:55:22 -0800

    Let Google users set their password

--- client/components/dashboard/user/Account.jsx
+++ client/components/dashboard/user/Account.jsx
@@ -1,31 +1,27 @@
+import { TextField, Button, Paper } from 'react-md';
 import request from 'superagent';
 import React from 'react';
 import swal from 'sweetalert';
 
-// react-md
-import TextField from 'react-md/lib/TextFields';
-import Button from 'react-md/lib/Buttons/Button';
-import Paper from 'react-md/lib/Papers';
-
 export default class UserAccount extends React.Component {
-  
+
   constructor(props) {
     super(props);
 
     this.state = { email: '', recovered: false };
   }
-  
+
   componentWillMount() {
     request
       .get('api/dashboard/user/account')
       .end((err, res) => this.setState(res.body));
   }
-  
+
   onUpdatePassword() {
     const curPass = this.refs.cpassword.value;
     const newPass = this.refs.npassword.value;
     const conPass = this.refs.rpassword.value;
-    
+
     if (newPass != conPass) {
       swal('Error', 'Passwords do not match.', 'error');
     }
@@ -44,30 +40,36 @@ export default class UserAccount extends React.Component {
         })
     }
   }
-  
+
   render() {
     return (
       <div className='dashboard-body user-account'>
         <Paper zDepth={1} className='section'>
           <h3 className='email'>{this.state.email}</h3>
-          
+
           <Button
             raised
             onClick={() => location.href = 'api/login/logout'}
           >Logout</Button>
         </Paper>
-        
-        <form
-          className='change-password md-paper md-paper--1 section flex'
-          style={{ display: this.state.google ? 'none' : '' }}
-        >
-          <h3>Change Password</h3>
+
+        <form className='change-password md-paper md-paper--1 section flex'>
+          <h3>Update Password</h3>
+          {this.state.google ? (
+            <p>
+              Your account was created via Google Sign-In and does not have a password set. On February 1st, Xyfir Accounts will no longer support Google Sign-In, meaning if you have not set a password by that date, your account will no longer be accessible. Once you set a password you will no longer be able to login via Google Sign-In and instead must use your email address and the password you set here.
+            </p>
+          ) : null}
 
           <TextField
             id='password--current'
             ref='cpassword'
+            type='password'
             label='Current Password'
-            style={{ display: this.state.recovered ? 'none' : 'initial' }}
+            style={{
+              display: this.state.recovered || this.state.google
+                ? 'none' : 'initial'
+            }}
             className='md-cell'
           />
           <TextField
@@ -93,5 +95,5 @@ export default class UserAccount extends React.Component {
       </div>
     );
   }
-  
+
 }
