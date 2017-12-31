be8d1934cc6619221050372b69fded43f26d2d50
MrXyfir <mr@xyfir.com>
2017-12-30 16:54:56 -0800

    Allow Google users to set password. Update code style

--- controllers/dashboard/user/account/update.js
+++ controllers/dashboard/user/account/update.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
-const mysql = require('lib/mysql');
+const MySQL = require('lib/mysql');
 
 /*
   PUT api/dashboard/user/account
@@ -11,48 +11,43 @@ const mysql = require('lib/mysql');
 module.exports = async function(req, res) {
 
   const { currentPassword, newPassword } = req.body;
-  const db = new mysql();
+  const db = new MySQL;
 
   try {
     await db.getConnection();
-
-    const rows = await db.query(
-      'SELECT password FROM users WHERE id = ?',
+    const [row] = await db.query(
+      'SELECT password, google FROM users WHERE id = ?',
       [req.session.uid]
     );
 
+    if (!row) throw 'Could not find user';
+
     // Check if current password matches
-    bcrypt.compare(currentPassword, rows[0].password, (err, match) => {
-      if (match || req.session.recovered) {
-        bcrypt.hash(newPassword, 10, (err, hash) => {
-          // Update password
-          db.query(
-            'UPDATE users SET password = ? WHERE id = ?',
-            [hash, req.session.uid]
-          )
-          .then(result => {
-            db.release();
-            res.json({
-              error: false, message: 'Password successfully updated'
-            });
-          })
-          .catch(err => {
-            db.release();
-            res.json({ error: true, message: 'An unknown error occurred' });
-          });
-        });
-        
-        req.session.recovered = false;
-      }
-      else {
-        db.release();
-        res.json({ error: true, message: 'Incorrect password' });
-      }
+    const match = await bcrypt.compare(currentPassword, row.password);
+
+    if (!match && !req.session.recovered && !row.google)
+      throw 'Incorrect password';
+
+    const hash = await bcrypt.hash(newPassword, 10);
+
+    // Update password
+    const result = await db.query(
+      'UPDATE users SET password = ?, google = 0 WHERE id = ?',
+      [hash, req.session.uid]
+    );
+    db.release();
+
+    if (!result.affectedRows) throw 'Could not update password';
+
+    res.json({
+      error: false, message: 'Password successfully updated'
     });
+
+    req.session.recovered = false;
   }
   catch (err) {
     db.release();
-    res.json({ error: true, message: 'An unknown error occurred' });
+    res.json({ error: true, message: err });
   }
 
 }
