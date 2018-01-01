dbdb102c8e6ff138afd119fe9da04fd01725f4d4
MrXyfir <mr@xyfir.com>
2018-01-01 09:44:39 -0800

    Improvements

--- jobs/google.js
+++ jobs/google.js
@@ -15,6 +15,8 @@ const MySQL = require('lib/mysql');
     );
     db.release();
 
+    console.log(`Notifying ${users.length} users`);
+
     for (let user of users) {
       try {
         await sendMail({
@@ -23,7 +25,7 @@ const MySQL = require('lib/mysql');
           html: `
             <p><strong>Warning!</strong> Your Xyfir Account (<code>${user.email}</code>) was created via Google Sign-In. You must update your account before you lose access.</p>
 
-            <p>Starting <strong>February 1st, 2018</strong>, Xyfir Accounts and all of the sites and applications it serves (Ptorx, xyBooks, xyAnnotations, etc) will no longer support Google Sign-In. This means that if you do not convert your account before that date you will lose access to your Xyfir Account and any of the sites linked to it.</p>
+            <p>Starting <strong>February 1st</strong>, Xyfir Accounts and all of the sites and applications it serves (Ptorx, xyBooks, xyAnnotations, etc) will no longer support Google Sign-In. This means that if you do not convert your account before that date you will lose access to your Xyfir Account and any of the sites linked to it.</p>
 
             <p>You can convert your Xyfir Account to a normal account by setting a password <a href="https://accounts.xyfir.com/#/dashboard/user/account">here</a>. You can login to your account via Google Sign-In until you set a password and from then on you will login using your email and password.</p>
 
