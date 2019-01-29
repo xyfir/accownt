c59c7410c65493e8cfcfd3ea8db1a1bf9c7c04ad
MrXyfir <mr@xyfir.com>
2019-01-29 14:08:01 -0800

    Remove admyn

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const express = require('express');
 const session = require('express-session');
 const parser = require('body-parser');
 const config = require('config');
-const admyn = require('admyn/server');
 const MySQL = require('lib/mysql');
 const app = express();
 
@@ -34,28 +33,6 @@ app.use(
   })
 );
 
-/* Admyn */
-app.use(
-  '/admyn-DU9oF5p690ojUKdR3NYS',
-  async function(req, res, next) {
-    // Load `users.admin` where user id
-    const db = new MySQL();
-    await db.getConnection();
-    const rows = await db.query('SELECT admin FROM users WHERE id = ?', [
-      req.session.uid
-    ]);
-    db.release();
-
-    if (!rows.length || !rows[0].admin) return res.status(403).send();
-    req.admyn = { database: config.db };
-    next();
-  },
-  admyn()
-);
-
-app.get('/admin-DU9oF5p690ojUKdR3NYS', (req, res) =>
-  res.sendFile(__dirname + '/views/Admin.html')
-);
 app.use('/api', require('./middleware/clean-email'), require('./controllers/'));
 app.get('/app', (req, res) => res.sendFile(__dirname + '/views/Redirect.html'));
 app.get('/*', (req, res) => res.sendFile(__dirname + '/views/App.html'));
--- web/components/Admin.jsx
+++
@@ -1,8 +0,0 @@
-import AdminPanel from 'admyn/client';
-import { render } from 'react-dom';
-import React from 'react';
-
-render(
-  <AdminPanel title="xyAccounts Admin" api="/admyn-DU9oF5p690ojUKdR3NYS/" />,
-  document.getElementById('content')
-);
