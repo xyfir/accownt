16725dbfd700f1a46d02817c44307709d2cca74f
MrXyfir <mr@xyfir.com>
2019-01-29 14:20:31 -0800

    Clean up code

--- server/app.js
+++ server/app.js
@@ -5,40 +5,25 @@ const express = require('express');
 const session = require('express-session');
 const parser = require('body-parser');
 const config = require('config');
-const MySQL = require('lib/mysql');
 const app = express();
 
-/* Serve Static Files */
 app.use('/static', express.static(__dirname + '/static'));
-
-/* Body-Parser */
 app.use(parser.json({ limit: '2mb' }));
 app.use(parser.urlencoded({ extended: true, limit: '2mb' }));
-
-/* Sessions */
 app.use(
   session({
     saveUninitialized: true,
-    store: new SessionStore({
-      host: config.db.host,
-      port: config.db.port,
-      user: config.db.user,
-      password: config.db.password,
-      database: config.db.database,
-      useConnectionPooling: true
-    }),
+    store: new SessionStore(config.db),
     secret: config.keys.session,
     resave: true,
     cookie: { httpOnly: false }
   })
 );
-
 app.use('/api', require('./middleware/clean-email'), require('./controllers/'));
-app.get('/app', (req, res) => res.sendFile(__dirname + '/views/Redirect.html'));
 app.get('/*', (req, res) => res.sendFile(__dirname + '/views/App.html'));
 
-app.listen(config.environment.port, () => {
-  console.log('Server running on port', config.environment.port);
-});
+app.listen(config.environment.port, () =>
+  console.log('Listening on', config.environment.port)
+);
 
 if (config.environment.runCronJobs) require('jobs/cron/start')();
