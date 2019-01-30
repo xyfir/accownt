fbc3713564c86f5490a17517954cdbdb1f189d87
MrXyfir <mr@xyfir.com>
2019-01-29 19:21:47 -0800

    Remove unused

--- server/app.js
+++ server/app.js
@@ -1,24 +1,15 @@
 require('app-module-path').addPath(__dirname);
 
-const SessionStore = require('express-mysql-session');
+const cookieParser = require('cookie-parser');
 const express = require('express');
-const session = require('express-session');
 const parser = require('body-parser');
 const config = require('config');
 const app = express();
 
 app.use('/static', express.static(__dirname + '/static'));
-app.use(parser.json({ limit: '2mb' }));
 app.use(parser.urlencoded({ extended: true, limit: '2mb' }));
-app.use(
-  session({
-    saveUninitialized: true,
-    store: new SessionStore(config.db),
-    secret: config.keys.session,
-    resave: true,
-    cookie: { httpOnly: false }
-  })
-);
+app.use(parser.json({ limit: '2mb' }));
+app.use(cookieParser());
 app.use('/api', require('./middleware/clean-email'), require('./controllers/'));
 app.get('/*', (req, res) => res.sendFile(__dirname + '/views/App.html'));
 
--- server/jobs/cron/start.js
+++
@@ -1,6 +0,0 @@
-const deleteExpiredAccessTokens = require('jobs/cron/delete-expired-access-tokens');
-
-module.exports = function() {
-  // Runs every 4 hours
-  setInterval(() => deleteExpiredAccessTokens(), 14400 * 1000);
-};
