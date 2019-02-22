e0b8945de1ce885549253db36db305edca75396c
MrXyfir <mr@xyfir.com>
2019-02-22 13:32:21 -0800

    Move 'parse process.env' code into its own file

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,5 @@
-// @ts-ignore
-process.ENV = Object.assign({}, process.env);
-Object.entries(process.ENV).forEach(e => {
-  if (typeof e[1] == 'string') {
-    if (e[1].startsWith('{') && e[1].endsWith('}')) {
-      try {
-        process.ENV[e[0]] = JSON.parse(e[1]);
-      } catch (err) {}
-    } else if (/^\d$/.test(e[1])) process.ENV[e[0]] = +e[1];
-    else if (e[1] == 'true') process.ENV[e[0]] = true;
-    else if (e[1] == 'false') process.ENV[e[0]] = false;
-  }
-});
-
 import 'app-module-path/register';
+import 'lib/utils/parse-env';
 import { verifyRequestJWT } from 'lib/jwt/verify';
 import * as cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
