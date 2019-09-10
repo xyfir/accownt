21955353891b6fdb0fbe444199d924c3b297075f
MrXyfir <mr@xyfir.com>
2019-09-09 20:28:32 -0700

    Fix test setup

--- server/lib/tests/setup.ts
+++ server/lib/tests/setup.ts
@@ -1,6 +1,21 @@
 import { config } from 'dotenv';
 config();
-
 import 'app-module-path/register';
 import 'jest-extended';
 import 'enve';
+
+import { emailToId } from 'lib/email/to-id';
+import * as storage from 'node-persist';
+
+const { TEST_STORAGE } = process.enve;
+
+beforeAll(async () => {
+  try {
+    const userId = await emailToId('test@example.com');
+    await storage.init(TEST_STORAGE);
+    await storage.removeItem(`user-${userId}`);
+    await storage.removeItem(`email-test@example.com`);
+  } catch (err) {
+    return;
+  }
+});
